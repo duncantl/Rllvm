ec7b2ff1431de48f6b09bca90bd164996b50397f
Duncan Temple Lang <duncan@r-project.org>
2014-07-10 19:33:06 -0700

    macros for constructing Pass objects

--- src/Pass.cpp
+++ src/Pass.cpp
@@ -37,6 +37,66 @@ R_initPassRegistry(SEXP r_registry)
 
 
 #include  <llvm/Transforms/Scalar.h>
+
+#define R_CREATE_FUNPASS(id) \
+extern "C" \
+SEXP  \
+R_##id() \
+{ \
+    llvm::FunctionPass *pass = llvm::id(); \
+    return(R_createRef(pass, "FunctionPass")); \
+}
+
+#define R_CREATE_PASS(id) \
+extern "C" \
+SEXP  \
+R_##id() \
+{ \
+    llvm::Pass *pass = llvm::id(); \
+    return(R_createRef(pass, "Pass")); \
+}
+
+R_CREATE_FUNPASS(createAggressiveDCEPass)
+R_CREATE_FUNPASS(createDeadCodeEliminationPass)
+R_CREATE_FUNPASS(createDeadStoreEliminationPass)
+R_CREATE_FUNPASS(createInstructionCombiningPass)
+R_CREATE_FUNPASS(createPromoteMemoryToRegisterPass)
+R_CREATE_FUNPASS(createDemoteRegisterToMemoryPass)
+
+R_CREATE_PASS(createLICMPass)
+R_CREATE_PASS(createSROAPass)
+R_CREATE_PASS(createScalarReplAggregatesPass)
+R_CREATE_PASS(createIndVarSimplifyPass)
+R_CREATE_PASS(createLoopStrengthReducePass)
+R_CREATE_PASS(createGlobalMergePass)
+R_CREATE_FUNPASS(createReassociatePass)
+R_CREATE_FUNPASS(createCFGSimplificationPass)
+R_CREATE_FUNPASS(createJumpThreadingPass)
+R_CREATE_FUNPASS(createTailCallEliminationPass)
+R_CREATE_FUNPASS(createFlattenCFGPass)
+
+R_CREATE_PASS(createStructurizeCFGPass)
+R_CREATE_PASS(createLoopSimplifyPass)
+R_CREATE_FUNPASS(createMemCpyOptPass)
+R_CREATE_FUNPASS(createCodeGenPreparePass)
+R_CREATE_FUNPASS(createInstructionSimplifierPass)
+R_CREATE_FUNPASS(createSinkingPass)
+
+R_CREATE_PASS(createLoopDeletionPass)
+R_CREATE_PASS(createCorrelatedValuePropagationPass)
+R_CREATE_PASS(createPartiallyInlineLibCallsPass)
+
+
+
+R_CREATE_PASS(createLoopUnswitchPass)
+R_CREATE_PASS(createLoopInstSimplifyPass)
+R_CREATE_PASS(createLoopUnrollPass)
+R_CREATE_PASS(createLoopRotatePass)
+R_CREATE_PASS(createLoopIdiomPass)
+
+
+
+#if 0
 extern "C"
 SEXP 
 R_createAggressiveDCEPass()
@@ -63,3 +123,11 @@ R_createDeadStoreEliminationPass()
 }
 
 
+extern "C"
+SEXP 
+R_createLICMPass()
+{
+    llvm::FunctionPass *pass = llvm::createLICMPass();
+    return(R_createRef(pass, "FunctionPass"));
+}
+#endif
