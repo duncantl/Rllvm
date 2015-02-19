01e4f47d08ff545ba6cb97f6005356d36a31eb5b
Duncan Temple Lang <duncan@r-project.org>
2015-02-19 08:14:11 -0800

    getSuccessors function

--- R/getCondition.R
+++ R/getCondition.R
@@ -7,13 +7,26 @@ function(x)
 getNumSuccessors =
 function(x)
 {
+  if(isa(x, "ReturnInst"))
+     return(0L)
+     
   .Call("R_BranchInst_getNumSuccessors", as(x, "BranchInst"))
 }
 
 getSuccessor =
 function(x, i = 1L)
 {
+  if(isa(x, "ReturnInst"))
+    return(NULL)
+     
   .Call("R_BranchInst_getSuccessor", as(x, "BranchInst"), as.integer(i - 1L))
+}
+
+getSuccessors =
+function(x)
+{
+  x = as(x, "BranchInst")
+  sapply(seq_len(getNumSuccessors(x)), function(i) getSuccessor(x, i))
 }
 
 isConditional =
