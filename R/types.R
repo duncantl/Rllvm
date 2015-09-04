0e9a86370dcaccfa3213e9221c580fd466546706
Duncan Temple Lang <duncan@r-project.org>
2015-09-04 11:02:41 -0700

    convert Type to character string

--- R/types.R
+++ R/types.R
@@ -18,6 +18,10 @@ setMethod("getDescription", "Type",
             function(x, ...)
                .Call("R_Type_getDescription", x))
 
+setAs("Type", "character",
+      function(from)
+        .Call("R_Type_print", from))
+
 
 TypeIDEnums = structure(c(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 
                            12L, 13L, 14L, 15L, 16L, 9L, 10L),
