29489fd1b6b0b180759a48cf71c5a93c93f7f344
Duncan Temple Lang <duncan@r-project.org>
2016-07-10 18:46:13 -0700

    better use of different execution engines to create different instances of modules

--- tests/clone.R
+++ tests/clone.R
@@ -3,15 +3,22 @@ InitializeNativeTarget()
 
 source('createGlobal.R')
 
+# Each time we create a new ExecutionEngine, we effectively reinitialize the
+# value of the global variable gv to -101
+
 ee = ExecutionEngine(mod)
 start = run(mod$get, .ee = ee)
 replicate(3, run(mod$foo, .ee = ee))
 end = run(mod$get, .ee = ee)
 
 m2 = clone(mod)
-start = run(m2$get, .ee = ee) # Note that this is 101
-replicate(10, run(m2$foo, .ee = ee))
-end = run(m2$get, .ee = ee)
+message("*** cloned module  **")
+showModule(m2)
+
+ee2 = ExecutionEngine(mod)
+start = run(m2$get, .ee = ee2) # Note that this is -101
+replicate(20, run(m2$foo, .ee = ee2))
+end = run(m2$get, .ee = ee2)
 
  # original module
 orig = run(mod$get, .ee = ee)
