5f5e50fec259bb81b104ee58dec18d684ee5d27f
Duncan Temple Lang <duncan@r-project.org>
2016-07-10 18:42:15 -0700

    arrange to create the bitcode

--- tests/readBitcode.R
+++ tests/readBitcode.R
@@ -2,6 +2,8 @@ library(Rllvm)
 source("tests/tut1.R") # create a module
 cat(as(mod, "character"), file = "bob.ll")
 # now create the bitcode.
+#
+system("make -f experiments/GNUmakefile bob.bc")
 m = readBitcode("bob.bc")
 names(m)
 run(m$foo, 2, 10, 3)
