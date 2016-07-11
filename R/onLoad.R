5c285b01b2526f5725f3a053c827f6c343a9775a
Duncan Temple Lang <duncan@r-project.org>
2016-07-11 10:57:59 -0700

    the VoidType, etc. are now of class Type not externalptr. Thanks to Christoph Klein

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,10 +1,14 @@
 getTypeDefs =
-function()
+function(namespace)
 {
    types = .Call("R_getTypeDefinitions")
+#   typeClass = getClass("Type", .Force = TRUE, where = namespace)
    names(types) = c("Void", "Label", "Float", "Double", "Int1", "Int8", "Int16", 
                      "Int32", "Int64", "FloatPtr", "DoublePtr", "Int32Ptr", "String")
-   types
+
+      # Make the Type objects, not externalptr objects. Thanks to Christoph Klein (doktorschiwago)
+      # for suggesting this.
+   lapply(types, function(x) new("Type", ref = x))
 }
 
 .onLoad =
@@ -13,14 +17,17 @@ function(...)
  if(getOption("Rllvm.autoInitialize", TRUE))
      InitializeNativeTarget()
     
-  types = getTypeDefs()
   e = getNamespace("Rllvm")
+  types = getTypeDefs(e)
+ 
   mapply(utils::assignInNamespace,
          paste(names(types), "Type", sep = ""),
          types, 
          MoreArgs = list(ns = e))
 
 # The StructType class isn't available yet at this stage in loading the package.
+#typeClass = getClass("StructType", .Force = TRUE, where = e)
+#typeClass = getClass("PointerType", .Force = TRUE, where = e)
   tmp =  makeSEXPTypes(rawPointer = TRUE)
   mapply(utils::assignInNamespace,
           c("SEXPType", sprintf("%sSXPType", names(tmp)[-1])),
