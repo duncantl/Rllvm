3af9d452337f1ad80abcf6a856fc65595385a9b7
Duncan Temple Lang <duncan@r-project.org>
2016-07-10 18:43:34 -0700

    use a different module IR and add more tests

--- tests/getUses.R
+++ tests/getUses.R
@@ -1,7 +1,14 @@
 library(Rllvm)
-m = parseIR("experiments/add_3.2.ll")
 
-b = getBlocks(m$foo)[[ 1 ]] 
+m = parseIR(system.file("IR", "fib.ll", package = "Rllvm"))
+b = getBlocks(m$fib1)
+length(b)
+sapply(b, getName)
+
+
+m1 = parseIR(system.file("IR", "add.ll", package = "Rllvm"))
+names(getBlocks(m1$foo))
+b = getBlocks(m1$foo)[[ 1 ]]
 
 getAllUses( b[[2]] )
 
