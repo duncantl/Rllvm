5163912425f47d14e90fce0ac68a59b00e9e4a99
Duncan Temple Lang <duncan@r-project.org>
2016-07-10 18:46:34 -0700

    better tests

--- tests/funcAttributes.R
+++ tests/funcAttributes.R
@@ -3,12 +3,6 @@ library(Rllvm)
 # Note that reading ll/ir or bitcode generated with llvm 3.3
 # into Rllvm using 3.2 will probably cause an assertion and termination
 
-m = parseIR(system.file("IR", "distance.ir", package = "Rllvm"))
-getFuncAttributes(m[["dist"]])
-setFuncAttributes(m[["dist"]], "NoUnwind")    # "Alignment", "NoCapture")
-showModule(m)
-getFuncAttributes(m[["dist"]])
-
 
 # Works with 3.3
 m = parseIR(system.file("IR", "add.ll", package = "Rllvm"))
@@ -22,3 +16,13 @@ f = Function('foo', Int32Type, Int32Type, m)
 getFuncAttributes(m[["foo"]])
 setFuncAttributes(m[["foo"]], "NoCapture")
 getFuncAttributes(m[["foo"]])
+
+
+# distance.ir is from an old version of LLVM.
+# Need to recreate via RLLVMCompile (or write the C code ourselves).
+m = parseIR(system.file("IR", "distance.ir", package = "Rllvm"))
+getFuncAttributes(m[["dist"]])
+setFuncAttributes(m[["dist"]], "NoUnwind")    # "Alignment", "NoCapture")
+showModule(m)
+getFuncAttributes(m[["dist"]])
+
