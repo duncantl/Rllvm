9fa5545f43276f6b5f4cb92aeea3c7fbb522457e
Duncan Temple Lang <duncan@r-project.org>
2016-07-10 18:37:31 -0700

    added a note about the purpose of this file

--- tests/verify.R
+++ tests/verify.R
@@ -1,9 +1,13 @@
 library(Rllvm)
 
+# The code we generate is intentionally broken so that we will fail the verifyModule call.
+# However, a Debug+Assert build of LLVM will fail in the createCall(ir, bar, ...)
+# as the call does not match the signature.
+
 mod = Module('example.ll')
 
 foo = Function('foo', Int32Type, list(), mod)
-bar = Function('bar', Int32Type, paramTypes=list(SEXPType), mod)
+bar = Function('bar', Int32Type, paramTypes = list( SEXPType ), mod)
 
 block = Block(foo, 'entry')
 ir = IRBuilder(block)
