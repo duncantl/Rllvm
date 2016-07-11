0265841206b1f7063383b2920f4abdbeb77b4fbf
Duncan Temple Lang <duncan@r-project.org>
2016-07-11 16:15:12 -0700

    test for running the optimizer on the code

---
+++ tests/optimize.R
@@ -0,0 +1,23 @@
+library(Rllvm)
+
+m = parseIR("bob.ll")
+
+fun = m$foo
+
+N = 1000
+tm1 = system.time(replicate(N, .llvm(fun, 3, 12, 4)))
+
+ee = ExecutionEngine(m)
+tm2 = system.time(replicate(N, .llvm(fun, 3, 12, 4, .ee = ee)))
+
+Optimize(m, ee)
+fun = m$foo
+tm3 = system.time(replicate(N, .llvm(fun, 3, 12, 4, .ee = ee)))
+
+
+
+
+
+
+       
+
