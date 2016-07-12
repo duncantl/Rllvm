f4ebac97426bd18e5f54b5490dc4cacf6fd56763
Duncan Temple Lang <duncan@r-project.org>
2016-07-11 20:12:00 -0700

    for exploring the C++ classes

---
+++ TU/exploreClasses.R
@@ -0,0 +1,7 @@
+# Source
+klasses = getCppClasses(tu)
+
+#Instruction classes
+grep("Inst$", names(k), value = TRUE)
+# Should check we have R classes defined for each of these and the C++ code in src/ refers to all of these.
+# in other words, identify which ones we are missing.
