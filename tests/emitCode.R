3b0026638e84d77771c238e6c9cee4ddbcda0e10
Duncan Temple Lang <duncan@r-project.org>
2016-07-24 11:39:31 -0700

    use enum name

--- tests/emitCode.R
+++ tests/emitCode.R
@@ -7,7 +7,7 @@ pm = passManager(mod) # Regular pass manager, not FunctionPassManager
 
 machine = createTargetMachine()
 fstream = raw_svector_ostream()
-status = addPassesToEmitFile(machine, pm, fstream, 0L)
+status = addPassesToEmitFile(machine, pm, fstream, Rllvm:::CGFT_AssemblyFile)
 
 run(pm, mod)
 
