61c0b43b1d65f5f2613df4e307631eca1796f213
Duncan Temple Lang <duncan@r-project.org>
2016-07-25 14:32:11 -0700

    computations to determine which enum symbols are overloaded/duplicated

--- TU/clang_new.R
+++ TU/clang_new.R
@@ -3,7 +3,7 @@ library(RCIndex)
 #*** Change the next two lines of code.
 
 version = c(3, 8)
-inc = c("/usr/local/include", "/usr/local/include/llvm")
+inc = c("/usr/local/include", "/usr/local/include/llvm", "/Users/duncan/R-3.3-devel/src/include")
 
 #inc= "/Users/duncan/LLVM3.7/clang+llvm-3.7.0-x86_64-apple-darwin/include"
 
@@ -12,7 +12,8 @@ f = "llvm.cpp"
 # "Compiler" arguments
 args = c("-xc++", "-DNDEBUG", "-D_GNU_SOURCE", "-D__STDC_CONSTANT_MACROS", "-D__STDC_FORMAT_MACROS", "-D__STDC_LIMIT_MACROS",
          "-std=c++11", "-fvisibility-inlines-hidden", "-fno-exceptions", "-fno-rtti", "-fno-common", "-Woverloaded-virtual",
-         "-Wcast-qual")
+         "-Wcast-qual",
+        "-DLLVM_VERSION=3", "-DLLVM_MINOR_VERSION=8", "-DNEW_LLVM_ATTRIBUTES_SETUP")
 
 
 # Parse the C++ code
@@ -20,12 +21,34 @@ tu = createTU(f, args = args, includes = inc, verbose = TRUE)
 
 options(nwarnings = 10000)
 
-# Get the enumerated constants
-enums = getEnums(tu)
+# Get the enumerated constants but just from the directorues with /llvm and /llvm-c
+enums = getEnums(tu, fileFilter = "/llvm") 
 
-if(FALSE) {
-   klasses = getCppClasses(tu)
-}
+
+ids = lapply(enums, function(x) names(x@values))
+tt = table(unlist(ids))
+sum(tt > 1)
+# Only 17 from the 279 different enum sets containing 9894 individual elements.
+
+dups = names(tt)[tt > 1]
+
+#w = sapply(enums, function(x)  x@values[match(dups, names(x@values), 0)])
+#d = enums[ sapply(w, length) > 0 ]
+
+# For each duplicate, find which enums define it.
+defs = sapply(dups, function(id)  names(enums)[ sapply(enums, function(x) id %in% names(x@values)) ])
+names(defs) = dups
+
+
+# We could analyze the code in ../src/ and see which routines and methods are using which enums.
+# Then we can limit the ones we generate code for to this.
+# Processing the code isn't impossible, but it requires knowing the class of the obj in obj->method.
+# This is typically encoded in the name of the routine, e.g., R_ExecutionEngine_addModule().
+#
+# However, we can also grep the .c/.cpp files
+#
+
+# For classes, see exploreClasses.R
 
 
 if(getOption("writeEnums", FALSE)) {
