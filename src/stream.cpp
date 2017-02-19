620b575fe141c69d61319a320482774538bf1359
Duncan Temple Lang <duncan@r-project.org>
2017-02-19 12:00:56 -0800

    fixed the issue with the assembler code having random garbage at the end

--- src/stream.cpp
+++ src/stream.cpp
@@ -9,9 +9,9 @@ extern "C"
 SEXP
 R_raw_svector_ostream()
 { 
-    llvm::SmallString<1000> *Str = new llvm::SmallString<1000>;
+    llvm::SmallString<10000> *Str = new llvm::SmallString<10000>;
     llvm::raw_svector_ostream *stream = new llvm::raw_svector_ostream(*Str);
-    return(R_createRef(stream, "raw_svector_ostream", "native symbol"));
+    return(R_createRef(stream, "raw_svector_ostream", "native symbol")); // XXX need finalizer.  Need to free Str also.
 }
 
 extern "C"
@@ -19,7 +19,7 @@ SEXP
 R_raw_svector_ostream_as_character(SEXP r_obj)
 {
     llvm::raw_svector_ostream *stream = GET_REF(r_obj, raw_svector_ostream);
-    return(ScalarString( mkChar( stream->str().data() )));
+    return(ScalarString( mkCharLen( stream->str().data(), stream->str().size() )));
 }
 
 
