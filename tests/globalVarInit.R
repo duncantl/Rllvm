cd4a15e59b64920fafb205796bc01e7a722d0a1f
Duncan Temple Lang <duncan@r-project.org>
2017-03-06 16:11:47 -0800

    gives warnings. Identify the stages of the module displays

--- tests/globalVarInit.R
+++ tests/globalVarInit.R
@@ -2,7 +2,12 @@ library(Rllvm)
 InitializeNativeTarget()
 mod = Module("global")
 mod[["status"]] = TRUE
+cat("Initial value\n")
 showModule(mod)
 setInitializer(mod[["status"]], createLogicalConstant(FALSE))
+cat("Second value\n")
 showModule(mod)
 mod[["status"]] = TRUE
+
+cat("Third value\n")
+showModule(mod)
