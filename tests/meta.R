db55d5f0b424e414402b562e9756d0e9fed0d915
Duncan Temple Lang <duncan@r-project.org>
2017-03-06 16:10:55 -0800

    runs

--- tests/meta.R
+++ tests/meta.R
@@ -1,9 +1,13 @@
 library(Rllvm)
 m = Module()
 
-#setMetadata(m, "foo", 1L)
-setMetadata(m, "bar", 3.1415)
+setMetadata(m, "foo", "1L")
+setMetadata(m, "bar", "3.1415")
+showModule(m)
+
 setMetadata(m, "str", "a string")
+
+setMetadata(m, "str2", list("a string", "and another"))
 
 # Fails with a cast error.
 #  !0 = !{Assertion failed: (isa<X>(Val) && "cast<Ty>() argument of incompatible type!"), function cast, file /Users/duncan/LLVM3.8/llvm-3.8.0.src/include/llvm/Support/Casting.h, line 237.
@@ -42,3 +46,12 @@ getValue(nmd)
 getValue(nmd[[1]][[1]])
 #.Call("R_convertValueToR", nmd[[1]][[1]])
 
+
+
+nmd = getMetadata(m, "str2")
+getNumOperands(nmd)
+md = nmd[[1]]
+getNumOperands(md)
+md[[1]]
+getValue(md[[1]])
+getValue(md[[2]])
