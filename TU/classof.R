c8bf83f78ab1af6b656c746d944fb5d6f3cce014
Duncan Temple Lang <duncan@r-project.org>
2018-03-30 16:17:43 -0700

    generate the C code for R function getClassName for turning a Value into a more specific class

---
+++ TU/classof.R
@@ -0,0 +1,23 @@
+# Probably don't need this as it is done in the R function
+# Rllvm:::coerceGenericInstruction using Op codes and a table.
+
+
+genClassofClassName = 
+function(classes, varName = "obj")
+{
+  c("const char const * getLLVMClassName(llvm::Value *obj)", "{",
+    '\tconst char *ans = "User";',
+    sprintf('\tif(llvm::%s::classof(obj))\n\t   ans = "%s";', classes, classes),
+    "\n\treturn(ans);",
+    "}")
+}
+
+
+
+getRValueClasses =
+function(base = "Value", where = "package:Rllvm")
+{
+  def = getClassDef(base, where = where)
+  subs = sapply(def@subclasses, slot, "subClass")
+  c(subs, unlist(lapply(subs, getRValueClasses, where = where)))
+}
