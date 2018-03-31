e2f172e96026caf7cce2ed5465e9a7cbc1bd1acd
Duncan Temple Lang <duncan@r-project.org>
2018-03-31 07:01:43 -0700

    getClassName() function

--- R/classof.R
+++ R/classof.R
@@ -4,3 +4,9 @@ function(obj, classname = class(obj))
   .Call("R_Instruction_classof", as(obj, "Instruction"), as.character(classname))
 }
 
+
+getClassName =
+function(value)
+{
+    .Call("R_Value_getClassName", value)
+}
