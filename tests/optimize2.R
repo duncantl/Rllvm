18ebdade22cdf03fb7320be505afe5776a1395de
Duncan Temple Lang <duncan@r-project.org>
2019-03-16 17:50:50 -0700

    need to use this to fix Rllvm.so when linking agains the binary builds from llvm.org

---
+++ tests/optimize2.R
@@ -0,0 +1,37 @@
+library(Rllvm)
+
+m = parseIR("bob.ll")
+
+#fun = m$foo
+
+showModule(m)
+
+ee = ExecutionEngine(m)
+mgr = getPassManager(m, ee, 3L)
+#  addPass(mgr, .Call("R_createInstructionCombiningPass"))
+  ## Eliminate Common SubExpressions.
+#  addPass(mgr, .Call("R_createGVNPass"))
+  # Reassociate expressions.
+  addPass(mgr, .Call("R_createReassociatePass"))
+  # Simplify the control flow graph (deleting unreachable blocks, etc).
+addPass(mgr, .Call("R_createCFGSimplificationPass"))
+addPass(mgr, .Call("R_createDeadCodeEliminationPass"))
+  addPass(mgr, .Call("R_createDeadStoreEliminationPass"))
+#createAggressiveDCEPass
+
+Optimize(m$foo, passManager = mgr)
+
+showModule(m)
+
+
+Optimize(m, ee)
+fun = m$foo
+tm3 = system.time(replicate(N, .llvm(fun, 3, 12, 4, .ee = ee)))
+
+
+
+
+
+
+       
+
