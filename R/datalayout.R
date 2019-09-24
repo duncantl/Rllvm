99fd57b36657abe6f311e0e46cdf219feec95d56
Duncan Temple Lang <duncan@r-project.org>
2019-09-24 08:37:50 -0700

    fixed cut-and-paste errors for dl when meant datalayou in function

--- R/datalayout.R
+++ R/datalayout.R
@@ -2,31 +2,31 @@
 isLittleEndian =
 function(datalayout)
 {
-    .Call("R_DataLayout_isLittleEndian", as(dl, "DataLayout"))
+    .Call("R_DataLayout_isLittleEndian", as(dataLayout, "DataLayout"))
 }
 
 getPointerSize =
 function(datalayout)
 {
-    .Call("R_DataLayout_getPointerSize", as(dl, "DataLayout"))
+    .Call("R_DataLayout_getPointerSize", as(dataLayout, "DataLayout"))
 }
 
 getPointerTypeSize =
 function(datalayout, type)
 {
-    .Call("R_DataLayout_getPointerTypeSize", as(dl, "DataLayout"), as(type, "Type"))
+    .Call("R_DataLayout_getPointerTypeSize", as(dataLayout, "DataLayout"), as(type, "Type"))
 }
 
 getTypeAllocSize =
 function(datalayout, type)
 {
-    .Call("R_DataLayout_getTypeAllocSize", as(dl, "DataLayout"), as(type, "Type"))
+    .Call("R_DataLayout_getTypeAllocSize", as(dataLayout, "DataLayout"), as(type, "Type"))
 }
 
 getStackAlignment = 
 function(datalayout)
 {
-    .Call("R_DataLayout_getStackAlignment", as(dl, "DataLayout"))
+    .Call("R_DataLayout_getStackAlignment", as(dataLayout, "DataLayout"))
 }
 
 
@@ -34,5 +34,5 @@ function(datalayout)
 getABITypeAlignment = 
 function(datalayout, type)
 {
-    .Call("R_DataLayout_getABITypeAlignment", as(dl, "DataLayout"), as(type, "Type"))
+    .Call("R_DataLayout_getABITypeAlignment", as(dataLayout, "DataLayout"), as(type, "Type"))
 }
