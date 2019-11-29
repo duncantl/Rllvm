0aac89cc0418101e117fbdd12abbd62ad7b76536
Duncan Temple Lang <duncan@r-project.org>
2019-11-29 09:08:03 -0800

    add drawCFG.R file and function. Also export isZeroValue

--- R/drawCFG.R
+++ R/drawCFG.R
@@ -4,15 +4,21 @@
 #
 
 
+#
+# Change the scale to separate the nodes
+# cat(drawCFG(mm$R_orderVector, range = c(-3, 3), x = 10), sep = "\n", file = "~/GitWorkingArea/Rllvm/AnalyzeCCode/icmp2CFG.tex")
+#
+# cat(drawCFG(mm$icmp, sep = "\n", file = "~/GitWorkingArea/Rllvm/AnalyzeCCode/icmp2CFG.tex")
+#
+
 drawCFG =
     #
     #  Assumes the BasicBlocks have names, i.e., -fno-discard-value-names
     #
     #
 function(fun, blocks = getBlocks(fun), ids = names(blocks),
-          x = 2, y = - seq(along = ids), text = ids)
+          x = 2, y = - seq(along = ids), text = ids, range = c(-1, 1))
 {
-
    tr = lapply(blocks, getTerminator)
    force(text)
    ids = fixId(ids)
@@ -21,7 +27,7 @@ function(fun, blocks = getBlocks(fun), ids = names(blocks),
    xs = ys = structure(rep(as.numeric(NA), length(ids)), names = ids)
    xs[1] = x
    ys[1] = 0
-#browser()   
+
    for( i in seq(along = xs)[-1] ) { # 2, 3, ...
        # Skip the first/entry block.
        # Branch, Return, Switch, TerminatorInst, cleanupret, callbrinvoke indirectbr catchswitch resume
@@ -38,20 +44,16 @@ function(fun, blocks = getBlocks(fun), ids = names(blocks),
        
        nx = fixId(sapply(els, getName))
        parent = fixId(getName(getParent(b)))
-#browser()
-#       if(parent == "if_end" ||  "if_end4" %in% nx) browser()
        w2 = is.na(xs[nx]) #  == 0
        if(length(nx) == 1) {
            xs[nx][w2] = xs[parent]
        } else {
-           xs[nx][w2] = (seq(-1, 1, length = length(nx)) + xs[parent])[w2]
+           xs[nx][w2] = (seq(range[1], range[2], length = length(nx)) + xs[parent])[w2]
        }
-       cat("Setting ", paste(nx[w2], collapse = ", "), "\n")
-
 
        ys[nx][is.na(ys[nx])] = ys[parent] - 1
    }
-#browser()       
+
     # determine the paths
    to = lapply(tr, function(x) {
                           els = x[]
@@ -65,6 +67,7 @@ function(fun, blocks = getBlocks(fun), ids = names(blocks),
                           } else
                             character()
                       })
+    # Find the sole return() call and put this at the center bottom.
    w = sapply(tr, is, "ReturnInst")
    xs[w] = x
    ys[w] = min(ys) - 1
@@ -73,8 +76,9 @@ function(fun, blocks = getBlocks(fun), ids = names(blocks),
    to2 = data.frame(from = rep(fixId(names(to)), sapply(to, length)), to = fixId(unlist(to)), stringsAsFactors = FALSE)
    bend = numeric(nrow(to2))
 
-   w =  abs(ys[to2$from] - ys[to2$to]) > 2 # 1
-   bend[w] = 90
+   h = ys[to2$from] - ys[to2$to]
+   w =  abs(h) > 2 # 1
+   bend[w] = 90* sign(h[w])
    to2$to[w] = sprintf("%s.east", to2$to[w])
    to2$from[w] = sprintf("%s.east", to2$from[w])   
    paths = sprintf("\\path [color=black] (%s) edge[ bend left=%.2f ] (%s);", to2$from, bend, to2$to)   
