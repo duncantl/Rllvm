52875bc53a52428509766acc8d1777f9635714ed
Duncan Temple Lang <duncan@r-project.org>
2020-02-06 15:04:34 -0800

    two files with newly added interface methods

---
+++ src/InvokeInst.cpp
@@ -0,0 +1,56 @@
+#include "Rllvm.h"
+
+
+extern "C"
+SEXP
+R_InvokeInst_getNormalDest(SEXP r_inst)
+{
+    llvm::InvokeInst *ins = GET_REF(r_inst, InvokeInst);
+    return(R_createRef(ins->getNormalDest(), "BasicBlock"));
+}
+
+extern "C"
+SEXP
+R_InvokeInst_getUnwindDest(SEXP r_inst)
+{
+    llvm::InvokeInst *ins = GET_REF(r_inst, InvokeInst);
+    return(R_createRef(ins->getUnwindDest(), "BasicBlock"));
+}
+
+extern "C"
+SEXP
+R_InvokeInst_doesNotThrow(SEXP r_inst)
+{
+    llvm::InvokeInst *ins = GET_REF(r_inst, InvokeInst);
+    return(ScalarLogical(ins->doesNotThrow()));
+}
+
+extern "C"
+SEXP
+R_InvokeInst_doesNoCfCheck(SEXP r_inst)
+{
+    llvm::InvokeInst *ins = GET_REF(r_inst, InvokeInst);
+    return(ScalarLogical(ins->doesNoCfCheck()));
+}
+
+
+
+extern "C"
+SEXP
+R_InvokeInst_setNormalDest(SEXP r_inst, SEXP r_dest)
+{
+    llvm::InvokeInst *ins = GET_REF(r_inst, InvokeInst);
+    llvm::BasicBlock *dest = GET_REF(r_inst, BasicBlock);
+    ins->setNormalDest(dest);
+    return(R_NilValue);
+}
+
+extern "C"
+SEXP
+R_InvokeInst_setUnwindDest(SEXP r_inst, SEXP r_dest)
+{
+    llvm::InvokeInst *ins = GET_REF(r_inst, InvokeInst);
+    llvm::BasicBlock *dest = GET_REF(r_inst, BasicBlock);
+    ins->setUnwindDest(dest);
+    return(R_NilValue);
+}
---
+++ src/LoadStore.cpp
@@ -0,0 +1,126 @@
+#include "Rllvm.h"
+
+#undef isUnordered
+
+#define isValue(type, method)                      \
+extern "C" \
+SEXP \
+R_##type##_##method(SEXP r_load) \
+{ \
+    llvm::type *ins = GET_REF(r_load, type); \
+    return(ScalarLogical(ins->method ())); \
+}
+
+
+isValue(LoadInst, isVolatile)
+isValue(LoadInst, isSimple)
+isValue(LoadInst, isUnordered)
+isValue(StoreInst, isVolatile)
+isValue(StoreInst, isSimple)
+isValue(StoreInst, isUnordered)
+
+
+
+
+SEXP
+unsigned_toR(unsigned val)
+{
+    return(ScalarReal(val));
+}
+
+SEXP
+AtomicOrdering_toR(llvm::AtomicOrdering val)
+{
+    const char *names[] = { "NotAtomic", // 0
+                          "Unordered", // 1
+                          "Monotonic", // 2, 
+                          "",  // 3
+                          "Acquire", // 4,
+                          "Release", // 5,
+                          "AcquireRelease" // 6,
+                          "SequentiallyConsistent"// = 7
+    };
+
+    SEXP ans;
+    PROTECT(ans = ScalarReal((int) val));
+    SET_NAMES(ans, ScalarString(mkChar(names[ (int) val])));
+    UNPROTECT(1);
+    return(ans);
+}
+
+SEXP
+llvmSyncScopeID_toR(llvm::SyncScope::ID val)
+{
+    return(ScalarInteger(val)); // Set the name.
+}
+
+#define getValue(type, method, retType)      \
+extern "C" \
+SEXP \
+R_##type##_get##method(SEXP r_inst) \
+{ \
+    llvm::type *ins = GET_REF(r_inst, type); \
+    return(retType##_toR(ins->get##method ()));      \
+}
+
+getValue(LoadInst, Alignment, unsigned)
+getValue(StoreInst, Alignment, unsigned)
+
+getValue(LoadInst, Ordering, AtomicOrdering)
+getValue(StoreInst, Ordering, AtomicOrdering)
+
+getValue(LoadInst, SyncScopeID, llvmSyncScopeID)
+getValue(StoreInst, SyncScopeID, llvmSyncScopeID)
+
+
+
+#define setValue(type, method, toType)      \
+extern "C" \
+SEXP \
+R_##type##_set##method(SEXP r_inst, SEXP r_val)   \
+{ \
+  llvm::type *ins = GET_REF(r_inst, type);        \
+  ins->set##method(toType##_fromR(r_val)); \
+  return(R_NilValue);      \
+}
+
+unsigned
+unsigned_fromR(SEXP r_val)
+{
+    return( ( unsigned) asReal(r_val) );
+}
+
+bool
+bool_fromR(SEXP r_val)
+{
+    return( ( bool) asLogical(r_val) );
+}
+
+llvm::AtomicOrdering
+AtomicOrdering_fromR(SEXP r_val)
+{
+    return( (llvm::AtomicOrdering) (INTEGER_VALUE(r_val)) );
+}
+
+llvm::SyncScope::ID
+SyncScopeID_fromR(SEXP r_val)
+{
+    return( (llvm::SyncScope::ID) (INTEGER_VALUE(r_val)) );
+}
+
+//setValue(LoadInst, Alignment, unsigned)
+//setValue(StoreInst, Alignment, unsigned)
+
+setValue(LoadInst, Volatile, bool)
+setValue(StoreInst, Volatile, bool)
+
+setValue(LoadInst, Ordering, AtomicOrdering)
+setValue(StoreInst, Ordering, AtomicOrdering)
+
+
+setValue(LoadInst, SyncScopeID, SyncScopeID)
+setValue(StoreInst, SyncScopeID, SyncScopeID)
+//setValue(StoreInst, Volatile, bool)
+
+
+
