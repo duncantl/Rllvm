95550a1ff6436fe300388f2fbe5b1afca0b0d591
Duncan Temple Lang <duncan@r-project.org>
2020-03-26 14:44:48 -0700

    test inserting elements via a GEP

---
+++ tests/insertElement.R
@@ -0,0 +1,14 @@
+library(Rllvm)
+m = Module()
+f = Function("foo", DoubleType, list(i = Int32Type, arr = pointerType(DoubleType)), module = m)
+b = Block(f)
+ir = IRBuilder(b)
+
+# just checking ir$createReturn(makeConstant(ir, 2.3))
+
+p = getParameters(f)
+v = ir$createLoad(p$arr)
+#v2 = ir$createExtractElement(v, 1L)
+#v2 = ir$createExtractElement(v, makeConstant(ir, 1L))
+v2 = ir$createGEP(v, c(0, 1L))
+ir$createReturn(v2)
