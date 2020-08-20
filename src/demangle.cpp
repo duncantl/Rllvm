2270995d875df45026041790e30313adfcb67e7b
Duncan Temple Lang <duncan@r-project.org>
2020-08-20 11:03:20 -0700

    enhance the demangle function to check the status of the demangling for each string in the vector and put the error name on it by if we want to query those that failed

--- src/demangle.cpp
+++ src/demangle.cpp
@@ -5,17 +5,23 @@
 
 extern "C"
 SEXP
-R_itaniumDemangle(SEXP r_string)
+R_itaniumDemangle(SEXP r_string, SEXP r_getStatus)
 {
     int n = Rf_length(r_string);
+    int getStatus = LOGICAL(r_getStatus)[0];
+    
     SEXP ans;
-    PROTECT(ans = NEW_STRING(n));
+    PROTECT(ans =  getStatus ? NEW_INTEGER(n) : NEW_STRING(n));
     for(int i = 0; i < n; i++) {
         char tmp[1000];
         int status = 0;
         size_t len = 1000;
+        memset(tmp, 0, len); // reset each time
         llvm::itaniumDemangle(CHAR(STRING_ELT(r_string, i)),  tmp, &len, &status);
-        SET_STRING_ELT(ans, i, mkChar(tmp));
+        if(getStatus)
+            INTEGER(ans)[i] = status;
+        else
+            SET_STRING_ELT(ans, i, status == 0? mkChar(tmp) : NA_STRING);
     }
     UNPROTECT(1);
     return(ans);
