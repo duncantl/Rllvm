65d462ece20188cdf0ce1e791017bd72d57f504e
Duncan Temple Lang <duncan@r-project.org>
2020-12-25 21:52:12 -0800

    extra checks

--- tests/mcjit.R
+++ tests/mcjit.R
@@ -8,15 +8,19 @@ finalizeEngine(ee)
 
 funptr = getPointerToFunction(m$fib, ee)
 
-n = 3:20
-N = 1e2L
- # Using the generated routine  from our LLVM module
+n = 3:20  # the values 
+N = 1e2L  # number of repetitions of calling the routine.
+
+# Using the generated routine  from our LLVM module.  But R_test_mcjit_fib is
+# in the Rllvm package.
 tm1 = system.time(a <- .Call("R_test_mcjit_fib", n, funptr@ref, N))
- # The pure C version.
+ # The pure C version from the Rllvm package.
 tm2 = system.time(b <- .Call("R_fib", n, N))
  # This has the overhead of creating the ExecutionEngine and generating the code.
 tm3 = system.time(c <- .Call("R_runModule", m, n, N))
 
+stopifnot(identical(a, b))
+stopifnot(identical(a, c))
 
 
 
