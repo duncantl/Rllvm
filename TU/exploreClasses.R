430ff604110b42cf7fb4eb6e8de509492ab87846
Duncan Temple Lang <duncan@r-project.org>
2020-12-25 22:02:21 -0800

    ...

--- TU/exploreClasses.R
+++ TU/exploreClasses.R
@@ -4,13 +4,49 @@ k = klasses = getCppClasses(tu, fileFilter = "/llvm/") # only the classes in /..
 
 sort(names(k))
 
+# 848
+
 
 #Instruction classes
 grep("Inst$", names(k), value = TRUE)
 
+# 71
+
 # Should check we have R classes defined for each of these and the C++ code in src/ refers to all of these.
 # in other words, identify which ones we are missing.
 z = getClasses(getNamespace("Rllvm"))
+
+# In LLVM but not in Rllvm
+miss = setdiff(names(k), z)
+# 628
+grep("Inst$", miss, value = TRUE)
+
+# SCEV - ScalarEvolutionExpressions
+#
+# CallGraph & CallGraphNode, Analysis & 2 passes.
+# CFGAnalyses
+# LazyCallGraph and friends.
+# CallSite, CallBase
+# PassManager
+# ConstantFolder
+# BlockAddress
+# EngineBuilder
+# ExecutionEngineState
+# LLVMTargetMachine
+# TargetOptions, MCTargetOptions
+# MemoryBuffer
+# Triple
+# GlobalIndirectSymbol
+# StructLayout
+# JITSymbol, JISTSymbolResolver
+# Error classes.
+# FunctionCallee
+# DebugLoc
+# IsValidReference, IsValidPointer, HasDererence
+# MetadataAsValue & ValueAsMetdata, ConstatnAsMetadata
+# AttrBuilder, AttributeList
+# ElementCount
+
 
 
 # Classes we have in R but not in LLVM
@@ -24,11 +60,11 @@ setdiff(z, names(k))
 
 # Find the files that we don't seem to have any entries from
 # Get a list of all the files under the llvm/ include directories
-af = list.files(grep("llvm", inc, value = TRUE), full.names = TRUE, recursive = TRUE)
+af = list.files(grep("llvm", inc[1], value = TRUE), full.names = TRUE, recursive = TRUE)
 
 # The names of the files from which we have class definitions
 ff = unique(sapply(k, function(x) getFileName(x@def)))
 
 m = setdiff(af, ff)
 # There are 672 files!
-
+# Now 2526! out of 2679 in af.
