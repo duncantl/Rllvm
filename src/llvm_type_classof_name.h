2c4c2bb75704ee809ac64ff0c6ca4ac371e52ec1
Duncan Temple Lang <duncan@r-project.org>
2020-12-25 20:16:18 -0800

    upddate for LLVM 11.0

--- src/llvm_type_classof_name.h
+++ src/llvm_type_classof_name.h
@@ -1,15 +1,41 @@
 /*
 source("fun.R")
+# How did we compute k? 
 o = getClassHierarchy("llvm::Type", k)
-tyClasses = c(unname(rev(unlist(o))), "llvm::SequentialType", "llvm::CompositeType", "llvm::Type")
+# The following is not general enough.  Need to include the  name of an intermediate
+# class, e.g. VectorType which has sub-classes.
+# Need to include 
+klasses = unique(c(unlist(o), names(o[[1]])))
+tyClasses = c(klasses, # "llvm::SequentialType", "llvm::CompositeType",
+               "llvm::Type")
 cat(paste(sprintf("   if(%s.classof(obj))\n\tans = \"%s\";\n", tyClasses, gsub("llvm::", "", tyClasses)), collapse = "   else "))
 */
-
 
 const char  * getLLVMTypeClassName(llvm::Type *obj)
 {
 
    const char *ans = "Type";
+
+#if LLVM_VERSION > 10
+   if(llvm::IntegerType.classof(obj))
+	ans = "IntegerType";
+   else    if(llvm::FunctionType.classof(obj))
+	ans = "FunctionType";
+   else    if(llvm::StructType.classof(obj))
+	ans = "StructType";
+   else    if(llvm::ArrayType.classof(obj))
+	ans = "ArrayType";
+   else    if(llvm::FixedVectorType.classof(obj))
+	ans = "FixedVectorType";
+   else    if(llvm::ScalableVectorType.classof(obj))
+	ans = "ScalableVectorType";
+   else    if(llvm::PointerType.classof(obj))
+	ans = "PointerType";
+   else    if(llvm::VectorType.classof(obj))
+	ans = "VectorType";
+   else    if(llvm::Type.classof(obj))
+	ans = "Type";
+#else
    if(llvm::PointerType::classof(obj))
 	ans = "PointerType";
    else    if(llvm::VectorType::classof(obj))
@@ -26,7 +52,7 @@ const char  * getLLVMTypeClassName(llvm::Type *obj)
 	ans = "SequentialType";
    else    if(llvm::CompositeType::classof(obj))
 	ans = "CompositeType";
-
+#endif
 
    return(ans);
 }
