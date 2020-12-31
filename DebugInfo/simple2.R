19c65333c0357be37ed43e6de5a820bfe71db3a0
Duncan Temple Lang <duncan@r-project.org>
2020-12-31 09:08:25 -0800

    calling function pointer is working

--- DebugInfo/simple2.R
+++ DebugInfo/simple2.R
@@ -11,26 +11,26 @@ offsets = structure(seq(along = fields) - 1L, names = names(fields))
 
 
 # Create a version of foo() named bar that does the exact same thing.
-bar = simpleFunction("bar", VoidType, mod = m) # n = Int32Type, mod = m)
+bar = simpleFunction("bar", Int32Type, mod = m) # n = Int32Type, mod = m)
 ir = bar$ir
 
 pvar = ir$createLoad(m[["pvar"]])
 
-if(FALSE) {
-gep = ir$createGEP(pvar, c(ir$createConstant(0L, Int32Type), ir$createConstant(offsets["f"], Int32Type))) # Not 64 for first element
-f = ir$createLoad(gep)
-z = ir$createCall(f, ir$createConstant(3L), ir$createConstant(10.5))
-} else {
-#gep = ir$createGEP(pvar, c(ir$createConstant(0L, Int32Type), ir$createConstant(offsets["v"], Int32Type))) # Not 64 for first element
-gep = ir$createGEP(pvar, c(0L, offsets["v"]))
+#gep = ir$createGEP(pvar, c(ir$createConstant(0L, Int32Type), ir$createConstant(offsets["f"], Int32Type))) # Not 64 for first element
+gep = ir$createGEP(pvar, c(0L, offsets["f"]))
 isInBounds(gep) =  TRUE
 f = ir$createLoad(gep)
-# ir$createBitCast(f, ?)
-#z = ir$createCall(f)
-}
+ans = ir$createCall(f, ir$createConstant(3L), ir$createConstant(10.5))
 
-ir$createReturn()
+ir$createReturn(ans)
 
+stopifnot(verifyModule(m))
 #.Call("R_Module_StripDebugInfo", m)
 showModule(m)
 
+
+ee = ExecutionEngine(m)
+.llvm(m$init, .ee = ee)
+.llvm(m$foo, .ee = ee)
+
+.llvm(m$bar, .ee = ee)
