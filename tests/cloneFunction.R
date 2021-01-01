ff820db5670ba3addf29411758893eeac0e35dfc
Duncan Temple Lang <duncan@r-project.org>
2020-12-31 17:42:57 -0800

    testing the cloning of a function into another module. Partially working

---
+++ tests/cloneFunction.R
@@ -0,0 +1,48 @@
+library(Rllvm)
+m = Module()
+v = Function("v", Int32Type, module = m)
+ir = IRBuilder(Block(v))
+ir$createReturn(ir$createConstant(3L))
+
+m2 = Module()
+v2 = Function("v2", Int32Type, module = m2)
+.Call("R_CloneFunctionInto", v, v2, FALSE)
+
+stopifnot(verifyModule(m2))
+stopifnot(.llvm(v2) == 3)
+
+# With parameters
+m = Module()
+v = Function("v", Int32Type, list(Int32Type, DoubleType), module = m)
+ir = IRBuilder(Block(v))
+ir$createReturn(ir$createConstant(3L))
+stopifnot(.llvm(v, 10, 3.5) == 3)
+
+
+m2 = Module()
+v2 = Function("v2", Int32Type, list(Int32Type, DoubleType), module = m2)
+.Call("R_CloneFunctionInto", v, v2, FALSE)
+stopifnot(verifyModule(m2))
+stopifnot(.llvm(v2, 10, 3.5) == 3)
+
+# If we don't create the parameters, we don't copy them.
+# Does the ValueToValueMapTy help with this.
+m2 = Module()
+v2 = Function("v2", Int32Type, module = m2)
+.Call("R_CloneFunctionInto", v, v2, FALSE)
+stopifnot(verifyModule(m2))
+stopifnot(.llvm(v2) == 3)
+
+
+
+copyFunction=
+function(fun, id, module = as(fun, "Module"))
+{
+    
+}
+
+setMethod("$<-", c("Module", value = "Function"),
+          function(x, name, value) {
+             
+          })
+
---
+++ tests/cloneFunction2.R
@@ -0,0 +1,12 @@
+library(Rllvm)
+
+m1 = parseIR(system.file("IR", "fib.ll", package = "Rllvm"))
+stopifnot(.llvm(m1$fib, 10) == 55)
+
+m2 = Module("other")
+f2 = Function("fib2",  getReturnType(m1$fib), getParameters(m1$fib), module = m2)
+.Call("R_CloneFunctionInto", m1$fib, f2, TRUE)
+
+stopifnot(verifyModule(m2))
+stopifnot(.llvm(f2, 10) == 55)
+
