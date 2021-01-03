4111448c8af7aaace56a39d9f5bbc9a1d2774285
Duncan Temple Lang <duncan@r-project.org>
2021-01-03 14:02:03 -0800

    using llvmDump() as what getTypeName() returns

--- R/typeName.R
+++ R/typeName.R
@@ -1,30 +1,43 @@
+#Wrote this one right below very quickly w/o realising there was one in Rllvm since not exported.
+# getTypeName = function(x) { ty = getType(x); if(isPointerType(ty)) paste0(getName(getElementType(ty)), " *") else getName(ty)}
+
+
+          
+
 getTypeName =
 function(ty, ...)
 {
+    if(is(ty, "Value"))
+        ty = getType(ty)
 
-  if(isPointerType(ty)) {
-    pty = getElementType(ty)
-    if(sameType(pty, Int8Type))
-      return("char *")
-    else
-      return(sprintf("%s *", getTypeName(pty)))
-  } else if(isStructType(ty))
-    return(sprintf("struct %s", gsub("^struct\\.", "", getStructTypeName(ty))))
+    return(llvmDump(ty))
+
+if(FALSE) {
+    if(isPointerType(ty)) {
+        pty = getElementType(ty)
+        if(sameType(pty, Int8Type))
+            return("char *")
+        else
+            return(sprintf("%s *", getTypeName(pty)))
+    } else if(isStructType(ty))
+        return(sprintf("struct %s", gsub("^struct\\.", "", getStructTypeName(ty))))
 
 
-   id = getTypeID(ty)
+    id = getTypeID(ty)
 
-  nm = if(id == IntegerTyID ) {
+    nm = if(id == IntegerTyID ) {
 
-         if(sameType(ty, Int32Type))
-           "int"
-         else
+        if(sameType(ty, Int32Type))
             "int"
-           
-       } else if(id == DoubleTyID) 
-         "double"
-       else if(id == DoubleTyID)
-         "float"  
+        else
+            "int"
+        
+    } else if(id == DoubleTyID) 
+        "double"
+    else if(id == DoubleTyID)
+        "float"  
 
-   nm
+    nm
+} # end if(FALSE)
+    
 }
