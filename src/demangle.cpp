e1ee42df1854ba32ec9fbb37a236639b51b5c461
Duncan Temple Lang <duncan@r-project.org>
2021-01-03 14:03:36 -0800

    enlarge the buffer from 1000 characters to 10,000. I had a crash because of the size. Should be dynamically done. Maybe when I have time

--- src/demangle.cpp
+++ src/demangle.cpp
@@ -13,9 +13,9 @@ R_itaniumDemangle(SEXP r_string, SEXP r_getStatus)
     SEXP ans;
     PROTECT(ans =  getStatus ? NEW_INTEGER(n) : NEW_STRING(n));
     for(int i = 0; i < n; i++) {
-        char tmp[1000];
+        char tmp[10000];  //XX HARD CODED. Went past the limit once with 1000 and crashed. Could happen again.
         int status = 0;
-        size_t len = 1000;
+        size_t len = 10000;
         memset(tmp, 0, len); // reset each time
         llvm::itaniumDemangle(CHAR(STRING_ELT(r_string, i)),  tmp, &len, &status);
         if(getStatus)
