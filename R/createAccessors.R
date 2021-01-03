72c9fff11d3d4e039553854b8bae89db503dcdef
Duncan Temple Lang <duncan@r-project.org>
2021-01-03 14:16:16 -0800

    code that generates get and set  access for GlobalVariable objects in a Module.

---
+++ R/createAccessors.R
@@ -0,0 +1,26 @@
+setGeneric("createAccessors", function(v, mod = NULL, ...) standardGeneric("createAccessors"))
+setMethod("createAccessors", "GlobalVariable",
+          function(v, mod = NULL, ...)
+          createAccessors(getName(v), as(v, "Module"), var = v))
+
+setMethod("createAccessors",
+           c("character", "Module"),
+          function(v, mod = as(var, "Module"), var = mod[[v]], ...) {
+
+              valType = getElementType(getType(var))
+
+              #XXX Handle the different types - doubles, strings, arrays, structs, pointers to structs, etc.
+              get = simpleFunction(paste0("get", v), valType, mod = mod)
+              ir = get$ir
+              ir$createReturn(ir$createLoad(var))
+
+              set = simpleFunction(paste0("set", v), VoidType, valType, mod = mod)
+              ir = set$ir
+              ir$createStore(set$params[[1]], var)
+              ir$createReturn()
+              
+
+              list(get = function(ee) .llvm(get$fun, .ee = ee),
+                   set = function(ee, val) .llvm(set$fun, val, .ee = ee))
+          })
+
