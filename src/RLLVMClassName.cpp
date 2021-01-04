783264ad64522954125cc507c06c3511dc19189b
Duncan Temple Lang <duncan@r-project.org>
2021-01-03 17:24:47 -0800

    the actual code to do the llvmISA calls

--- src/RLLVMClassName.cpp
+++ src/RLLVMClassName.cpp
@@ -12,7 +12,7 @@ R_Value_getClassName(SEXP r_val)
     return(ScalarString(mkChar(getLLVMClassName(val))));
 }
 
-
+#if 0
 extern "C"
 SEXP
 R_isBasicBlock(SEXP r_val)
@@ -20,3 +20,23 @@ R_isBasicBlock(SEXP r_val)
     llvm::Value * val = GET_REF(r_val, Value);
     return(ScalarLogical(llvm::BasicBlock::classof(val)));
 }
+#endif
+
+#define LLVM_isA(type) \
+extern "C" \
+SEXP \
+R_isA_##type (SEXP r_val) \
+{  \
+    llvm::Value * val = GET_REF(r_val, Value); \
+    return(ScalarLogical(llvm::type::classof(val)));  \
+}
+
+
+// ?? #include <llvm/IR/DebugInfo.h>
+#include <llvm/IR/DerivedUser.h>
+#include <llvm/IR/IntrinsicInst.h>
+#include <llvm/Analysis/MemorySSA.h>
+
+// LLVM_isA(BitCastInst)
+#include "LLVM_isA.h"
+
