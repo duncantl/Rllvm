e0d92b1387840647bce801a2c12b3ff8d8f160ad
Duncan Temple Lang <duncan@r-project.org>
2021-01-11 20:19:09 -0800

    example of a Module containing R signatures

---
+++ inst/IR/rapiEg.c
@@ -0,0 +1,10 @@
+#include <Rdefines.h>
+
+void *_[] = {
+    (void*) Rf_allocVector,
+    (void*) Rf_setAttrib,
+    (void*) SET_VECTOR_ELT,
+    (void*) SET_STRING_ELT,    
+    (void*) REAL,
+    (void*) INTEGER,        
+};
