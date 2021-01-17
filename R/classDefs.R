08c1e9d3d456aad74c7930f84c774144188ad546
Duncan Temple Lang <duncan@r-project.org>
2021-01-16 21:15:55 -0800

    moving setClass and setGeneric code out of classDefs.R

--- R/classDefs.R
+++ R/classDefs.R
@@ -24,15 +24,10 @@ setClass("IRBuilder", contains = "RC++Reference")
 setClass("LLVMContext", contains = "RC++Reference")
 setClass("ExecutionEngine", contains = "RC++Reference")
 setClass("Value", contains = "RC++Reference")
-setClass("BasicBlock", contains = "Value")
 
+setClass("MemSetInst", contains = "RC++Reference")
 
 setClass("DIBuilder", contains = "RC++Reference")
-setClass("DICompileUnit", contains = "RC++Reference")
-setClass("DISubprogram", contains = "RC++Reference")
-setClass("DIType", contains = "RC++Reference")
-setClass("DISubroutineType", contains = "RC++Reference")
-
 
 
 setAs("Value", "character",
@@ -41,257 +36,21 @@ setAs("Value", "character",
 
 
 
-
-setClass("Argument", contains = "Value")
-
 setClass("PassManagerBase", contains = "RC++Reference")
 setClass("LegacyPassManager", contains = "PassManagerBase")
 
-# if llvm 3.1, otherwise Instruction extends RC++Reference
-# setClass("Instruction", contains = "RC++Reference")
-setClass("User", contains = "Value")
-setClass("Instruction", contains = "User")
 
 setClass("Use", contains = "RC++Reference")
-
-setClass("UnaryInstruction", contains = "Instruction")
-setClass("UnaryOperator", contains = "UnaryInstruction")
-
-#setClass("ReturnInst", contains = "TerminatorInst")
-setClass("CallInst", contains = "Instruction")
-setClass("CastInst", contains = "UnaryInstruction")
-setClass("SExtInst", contains = "CastInst")
-
-
-setClass("IndirectBrInst", contains = "Instruction")
-
-setClass("BinaryOperator", contains = "Instruction")
-
-# LLVM 5.0.1 at least.
-setClass("Operator", contains = "User")
-setClass("OverflowingBinaryOperator", contains = "Operator")
-setClass("FPMathOperator", contains = "Operator")
-
-
-
-setClass("ParameterList", representation(names = "character"), contains = "list")
-
-  # allocates memory on the stack.
-setClass("AllocaInst", contains = "UnaryInstruction")
-setClass("StoreInst", contains = "Instruction")
-setClass("LoadInst", contains = "Instruction")
-
-setClass("GetElementPtrInst", contains = "Instruction")
-setClass("PtrDiff", contains = "Value")  # was Instruction, but looks incorrect.
-
-
-#XXXX remove TerminatorInst
-setClass("TerminatorInst", contains = "Instruction")
-setClass("BranchInst", contains = "TerminatorInst")
-setClass('IndirectBrInst', contains = 'TerminatorInst')
-setClass("InvokeInst", contains = "TerminatorInst")
-setClass("ReturnInst", contains = "TerminatorInst")
-setClass("SwitchInst", contains = "TerminatorInst")
-setClass('UnreachableInst', contains = 'TerminatorInst')
-setClass('UnwindInst', contains = 'TerminatorInst')
-
-setClass('ResumeInst', contains = 'Instruction')
-
-setClass('FenceInst', contains = 'Instruction')
-setClass('AtomicCmpXchgInst', contains = 'Instruction')
-setClass('AtomicRMWInst', contains = 'Instruction')
-setClass('TruncInst', contains = 'CastInst')
-setClass('ZExtInst', contains = 'CastInst')
-setClass('FPToUIInst', contains = 'CastInst')
-setClass('FPToSIInst', contains = 'CastInst')
-setClass('UIToFPInst', contains = 'CastInst')
-setClass('SIToFPInst', contains = 'CastInst')
-setClass('FPTruncInst', contains = 'CastInst')
-setClass('FPExtInst', contains = 'CastInst')
-setClass('PtrToIntInst', contains = 'CastInst')
-setClass('IntToPtrInst', contains = 'CastInst')
-setClass('BitCastInst', contains = 'CastInst')
-setClass('AddrSpaceCastInst', contains = 'CastInst')
-setClass('CmpInst', contains = 'Instruction')
-setClass('ICmpInst', contains = 'CmpInst')
-setClass('FCmpInst', contains = 'CmpInst')
-setClass('PHINode', contains = 'Instruction')
-setClass('SelectInst', contains = 'Instruction')
-setClass('VAArgInst', contains = 'UnaryInstruction')
-setClass('ExtractElementInst', contains = 'Instruction')
-setClass('InsertElementInst', contains = 'Instruction')
-setClass('ShuffleVectorInst', contains = 'Instruction')
-setClass('ExtractValueInst', contains = 'UnaryInstruction')
-setClass('InsertValueInst', contains = 'Instruction')
-setClass('LandingPadInst', contains = 'Instruction')
-
-setClass("CatchSwitchInst", contains = "Instruction")
-setClass("FuncletPadInst", contains = "Instruction")
-setClass("CleanupPadInst", contains = "FuncletPadInst")
-setClass("CatchPadInst", contains = "FuncletPadInst")
-setClass("CatchReturnInst", contains = "Instruction")
-setClass("CleanupReturnInst", contains = "Instruction")
 
 setClass("NamedMDNode", contains = "RC++Reference")
 setClass("Metadata", contains = "RC++Reference")
 setClass("MDNode", contains = "Metadata") # "Value")
 setClass("MDString", contains = "Metadata") # "Value")
 
-
-setClass("DINode", contains = "MDNode")
-setClass("DIGlobalVariableExpression", contains = "MDNode")
-setClass("GenericDINode", contains = "DINode")
-setClass("DISubrange", contains = "DINode")
-setClass("DIEnumerator", contains = "DINode")
-setClass("DIScope", contains = "DINode")
-setClass("DIFile", contains = "DINode")
-setClass("DIType", contains = "DIScope")
-setClass("DIBasicType", contains = "DIType")
-setClass("DIDerivedType", contains = "DIType")
-setClass("DICompositeType", contains = "DIType")
-setClass("DISubroutineType", contains = "DIType")
-setClass("DICompileUnit", contains = "DIScope")
-setClass("DILocalScope", contains = "DIScope")
-setClass("DILocation", contains = "MDNode")
-setClass("DISubprogram", contains = "DILocalScope")
-setClass("DILexicalBlockBase", contains = "DILocalScope")
-setClass("DILexicalBlock", contains = "DILexicalBlockBase")
-setClass("DILexicalBlockFile", contains = "DILexicalBlockBase")
-
-
-
 setClass("Type", contains = "RC++Reference")
-setClass("StructType", contains = "Type")# was CompositeType
-setClass("StructTypeWithNames", representation(names = "character"), contains = "StructType")
-
-
-setClass("PointerType", contains = "Type")
-setClass("ArrayType", contains = "Type")
-setClass("VectorType", contains = "Type")
-
-
-
-setClass("FunctionType", contains = "Type")
-setClass("IntegerType", contains = "Type")
-setClass("DoubleType", contains = "Type") #XXX Not in LLVM
-
- # A separate class so we can identify a StringType from a generic pointer since now they both use i8*
-setClass("StringType", contains = "Type") 
-
-setMethod("show", "StringType",
-          function(object) {
-              cat(class(object), "\n")
-          })
-
-
-# R specific types
-setClass("SEXPType", contains = "PointerType")
-setClass("LGLSXPType", contains = "SEXPType")
-setClass("INTSXPType", contains = "SEXPType")
-setClass("REALSXPType", contains = "SEXPType")
-setClass("STRSXPType", contains = "SEXPType")
-setClass("VECSXPType", contains = "SEXPType")
-setClass("CHARSXPType", contains = "SEXPType")
-
-setMethod("show", "SEXPType",
-          function(object) {
-              cat(class(object), "\n")
-          })
-
-
-
-
-setClass("Constant", contains = "User")
-setClass("ConstantAggregateZero", contains = "Constant")
-setClass("ConstantInt", contains = "Constant")
-setClass("ConstantFP", contains = "Constant")
-setClass("GlobalValue", contains = "Constant")
-setClass("GlobalVariable", contains = "GlobalValue")
-
-setClass("ConstantPointerNull", contains = "Constant")
-
-setClass("ConstantAggregate", contains = "Constant")
-setClass("ConstantArray", contains = "ConstantAggregate")
-setClass("ConstantStruct", contains = "ConstantAggregate")
-
-setAs("Value", "ConstantStruct", function(from) new("ConstantStruct", ref = from@ref))
-
-
-setClass("ConstantExpr", contains = "User")
-setClass("BinaryConstantExpr", contains = "ConstantExpr")
-setClass("CompareConstantExpr", contains = "ConstantExpr")
-setClass("ExtractElementConstantExpr", contains = "ConstantExpr")
-setClass("ExtractValueConstantExpr", contains = "ConstantExpr")
-setClass("GetElementPtrConstantExpr", contains = "ConstantExpr")
-setClass("InsertElementConstantExpr", contains = "ConstantExpr")
-setClass("InsertValueConstantExpr", contains = "ConstantExpr")
-setClass("SelectConstantExpr", contains = "ConstantExpr")
-setClass("ShuffleVectorConstantExpr", contains = "ConstantExpr")
-setClass("UnaryConstantExpr", contains = "ConstantExpr")
-
-setAs("ConstantExpr", "Instruction",
-      function(from) {
-          ans = .Call("R_ConstantExpr_getAsInstruction", from)
-          as(ans, getClassName(ans))
-      })
-
-
-setClass("ConstantData", contains = "Constant")
-setClass("ConstantDataSequential", contains = "ConstantData")
-setClass("ConstantDataArray", contains = "ConstantDataSequential")
-setClass("ConstantDataVector", contains = "ConstantDataSequential")
-
-setClass("Function", contains = "GlobalValue")
 
 setClass("NativeFunctionPointer", contains = "RC++Reference")
 setClass("NativeGlobalVariable", contains = "RC++Reference")
-
-
-
-
-
-setMethod("show", "Type",
-          function(object) {
-            cat("Type", names(getTypeID(object)), "\n")
-          })
-
-setMethod("show", "Function",
-          function(object) {
-
-            p = getParameters(object)
-            names(p)[is.na(names(p))] = ""
-            sig = sprintf("%s %s", sapply(p, function(x) getTypeName(getType(x))), names(p))
-            
-            cat(getTypeName(getReturnType(object)),
-                getName(object), "(",
-                paste(sig, collapse = ", "),
-                ")\n")
-          })
-
-setMethod("show", "Module",
-          function(object) {
-            cat("Module:", getName(object), "\n",
-                 "  contents: ", paste(names(object), sep = ", "), "\n")
-          })
-
-
-setGeneric("setName",
-function(obj, name, ...)
-           standardGeneric("setName"))
-
-setGeneric("getName",
-function(obj, ...)
-           standardGeneric("getName"))
-
-#??? Do we want this?
-setMethod("getName", "NULL", function(obj, ...) NA)
-
-
-setGeneric("clone", function(x, ...) standardGeneric("clone"))
-
-setAs("Module", "ExecutionEngine", 
-       function(from)
-          ExecutionEngine(from))
 
 
 setClass("Target", contains = "RC++Reference")
@@ -304,10 +63,6 @@ setClass("Pass", contains = "RC++Reference")
 setClass("FunctionPass", contains = "RC++Reference")
 setClass("ImmutablePass", contains = "Pass")
 setClass("DataLayout", contains = "ImmutablePass") 
-
-setGeneric("getDataLayout", function(from, ...) standardGeneric("getDataLayout"))
-setGeneric("setDataLayout", function(x, value, ...) standardGeneric("setDataLayout"))
-
 
 
 llvmISA =
@@ -326,155 +81,13 @@ llvmISA =
       
 
 
-setGeneric("llvmDump", function(x, ...) standardGeneric("llvmDump"))
-
-setMethod("llvmDump", "Value",
-           function(x, ...) 
-               structure(.Call("R_Value_dump", x), class = "LLVMDump"))
-
-setMethod("llvmDump", "Type",
-           function(x, ...) 
-               .Call("R_Type_dump", x))
-
-print.LLVMDump =
-function(x, ...)
-    cat(x, "\n")
-
-
-setGeneric("getValue", function(x, ...) standardGeneric("getValue"))
-
-
-setMethod("getValue", "Value",
-           function(x, ...)
-              .Call("R_convertValueToR", x))
-
-setMethod("getValue", "MDNode",
-           function(x, ...) {
-             sapply(seq(1, length = getNumOperands(x)),
-                     function(i)
-                         getValue(x[[i]]))
-           })
-
-setMethod("getValue", "NamedMDNode",
-           function(x, ...) {
-             sapply(seq(1, length = getNumOperands(x)),
-                     function(i)
-                         getValue(x[[i]]))
-           })
-          
-
-# experiment. May change.
-setMethod("getValue", "Metadata",
-           function(x, ...) {
-              gsub("^!", "",  as(x, "character"))
-           })
-
-setGeneric("onlyReadsMemory",
-            function(x, ...)
-                standardGeneric("onlyReadsMemory"))
-
-
-setMethod("onlyReadsMemory", "Function",
-            function(x, ...)
-                .Call("R_Function_onlyReadsMemory", x))
-setMethod("onlyReadsMemory", "Argument",
-            function(x, ...)
-                .Call("R_Argument_onlyReadsMemory", x))
-
-
-
-setGeneric("getMetadata",
-           function(obj, id, ...)
-              standardGeneric("getMetadata"))
-
-
-
-setMethod("show", "Value", function(object) print(as(object,'character')))
-
-
-
-setGeneric("getPredecessors", function(x, ...) standardGeneric("getPredecessors"))
-setGeneric("getSuccessors", function(x, ...) standardGeneric("getSuccessors"))
-setGeneric("getSuccessor", function(x, ...) standardGeneric("getSuccessor"))
-setGeneric("getPredecessor", function(x, ...) standardGeneric("getPredecessor"))
-
-
-
-
-# For DI nodes - debug info. But isLittleEndian is used for datalayout.
-setGeneric("isPrivate", function(x, ...) standardGeneric("isPrivate"))
-setGeneric("isProtected", function(x, ...) standardGeneric("isProtected"))
-setGeneric("isPublic", function(x, ...) standardGeneric("isPublic"))
-setGeneric("isForwardDecl", function(x, ...) standardGeneric("isForwardDecl"))
-setGeneric("isAppleBlockExtension", function(x, ...) standardGeneric("isAppleBlockExtension"))
-setGeneric("isVirtual", function(x, ...) standardGeneric("isVirtual"))
-setGeneric("isArtificial", function(x, ...) standardGeneric("isArtificial"))
-setGeneric("isObjectPointer", function(x, ...) standardGeneric("isObjectPointer"))
-setGeneric("isObjcClassComplete", function(x, ...) standardGeneric("isObjcClassComplete"))
-setGeneric("isVector", function(x, ...) standardGeneric("isVector"))
-setGeneric("isBitField", function(x, ...) standardGeneric("isBitField"))
-setGeneric("isStaticMember", function(x, ...) standardGeneric("isStaticMember"))
-setGeneric("isLValueReference", function(x, ...) standardGeneric("isLValueReference"))
-setGeneric("isRValueReference", function(x, ...) standardGeneric("isRValueReference"))
-setGeneric("isTypePassByValue", function(x, ...) standardGeneric("isTypePassByValue"))
-setGeneric("isTypePassByReference", function(x, ...) standardGeneric("isTypePassByReference"))
-setGeneric("isBigEndian", function(x, ...) standardGeneric("isBigEndian"))
-setGeneric("isLittleEndian", function(x, ...) standardGeneric("isLittleEndian"))
-
-
-setGeneric("getElements",
-           function(x, ...)
-           standardGeneric("getElements"))
-
-
-setGeneric("getEmissionKind",
-           function(x, ...)
-           standardGeneric("getEmissionKind"))
-
-setGeneric("isOptimized",
-           function(x, ...)
-           standardGeneric("isOptimized"))
-
-setGeneric("getLine",
-           function(x, ...)
-           standardGeneric("getLine"))
-
-setGeneric("getNameTableKind", function(x, ...) standardGeneric("getNameTableKind"))
-setGeneric("stripDebugInfo", function(x, ...) standardGeneric("stripDebugInfo"))
-
-
-setGeneric("getTypes", function(x, ...) standardGeneric("getTypes"))
-
-setGeneric("getFilename", function(x, ...) standardGeneric("getFilename"))
-setGeneric("getDirectory", function(x, ...) standardGeneric("getDirectory"))
-
-
-
-
-setGeneric("getType",
-            function(obj, ...)
-              standardGeneric("getType"))
-
-
-
-
 # Retired from LLVM.
 # See note in NAMESPACE as to why this is still here - temporarily
 setClass("FunctionPassManager", contains = "PassManagerBase")
-if(FALSE) {
-setClass("Mangler", contains = "RC++Reference")
-setClass("DerivedType", contains = "Type")    
-setClass("CompositeType", contains = "DerivedType")
-setClass("UnionType", contains = "CompositeType")
-setClass("SequentialType", contains = "CompositeType")
-setClass("formatted_raw_ostream", contains = "raw_ostream")
-setClass("TerminatorInst", contains = "Instruction")
-}
+
+
+setClass("ParameterList", representation(names = "character"), contains = "list")
 
 
 
 
-# setGeneric("getExternalFunctions", function(x, ...) standardGeneric("getExternalFunctions"))
-# See findCalledFunctions.R
-
-setGeneric("hasError", function(x, ...) standardGeneric("hasError"))
--- R/manual_generics.R
+++ R/manual_generics.R
@@ -38,3 +38,98 @@ setGeneric("isZeroValue", function(x, ...) standardGeneric("isZeroValue"))
 
 setGeneric("getInstructions", function(x, debug = TRUE, ...) standardGeneric("getInstructions"))
 
+
+
+setGeneric("setName",
+function(obj, name, ...)
+           standardGeneric("setName"))
+
+setGeneric("getName",
+function(obj, ...)
+           standardGeneric("getName"))
+
+
+setGeneric("clone", function(x, ...) standardGeneric("clone"))
+
+
+setGeneric("getDataLayout", function(from, ...) standardGeneric("getDataLayout"))
+
+setGeneric("setDataLayout", function(x, value, ...) standardGeneric("setDataLayout"))
+
+
+
+setGeneric("llvmDump", function(x, ...) standardGeneric("llvmDump"))
+
+setGeneric("getValue", function(x, ...) standardGeneric("getValue"))
+
+
+
+setGeneric("onlyReadsMemory",
+            function(x, ...)
+                standardGeneric("onlyReadsMemory"))
+
+setGeneric("getMetadata",
+           function(obj, id, ...)
+              standardGeneric("getMetadata"))
+
+setGeneric("getPredecessors", function(x, ...) standardGeneric("getPredecessors"))
+setGeneric("getSuccessors", function(x, ...) standardGeneric("getSuccessors"))
+setGeneric("getSuccessor", function(x, ...) standardGeneric("getSuccessor"))
+setGeneric("getPredecessor", function(x, ...) standardGeneric("getPredecessor"))
+
+
+# For DI nodes - debug info. But isLittleEndian is used for datalayout.
+setGeneric("isPrivate", function(x, ...) standardGeneric("isPrivate"))
+setGeneric("isProtected", function(x, ...) standardGeneric("isProtected"))
+setGeneric("isPublic", function(x, ...) standardGeneric("isPublic"))
+setGeneric("isForwardDecl", function(x, ...) standardGeneric("isForwardDecl"))
+setGeneric("isAppleBlockExtension", function(x, ...) standardGeneric("isAppleBlockExtension"))
+setGeneric("isVirtual", function(x, ...) standardGeneric("isVirtual"))
+setGeneric("isArtificial", function(x, ...) standardGeneric("isArtificial"))
+setGeneric("isObjectPointer", function(x, ...) standardGeneric("isObjectPointer"))
+setGeneric("isObjcClassComplete", function(x, ...) standardGeneric("isObjcClassComplete"))
+setGeneric("isVector", function(x, ...) standardGeneric("isVector"))
+setGeneric("isBitField", function(x, ...) standardGeneric("isBitField"))
+setGeneric("isStaticMember", function(x, ...) standardGeneric("isStaticMember"))
+setGeneric("isLValueReference", function(x, ...) standardGeneric("isLValueReference"))
+setGeneric("isRValueReference", function(x, ...) standardGeneric("isRValueReference"))
+setGeneric("isTypePassByValue", function(x, ...) standardGeneric("isTypePassByValue"))
+setGeneric("isTypePassByReference", function(x, ...) standardGeneric("isTypePassByReference"))
+setGeneric("isBigEndian", function(x, ...) standardGeneric("isBigEndian"))
+setGeneric("isLittleEndian", function(x, ...) standardGeneric("isLittleEndian"))
+
+
+setGeneric("getElements",
+           function(x, ...)
+           standardGeneric("getElements"))
+
+
+setGeneric("getEmissionKind",
+           function(x, ...)
+           standardGeneric("getEmissionKind"))
+
+setGeneric("isOptimized",
+           function(x, ...)
+           standardGeneric("isOptimized"))
+
+setGeneric("getLine",
+           function(x, ...)
+           standardGeneric("getLine"))
+
+setGeneric("getNameTableKind", function(x, ...) standardGeneric("getNameTableKind"))
+setGeneric("stripDebugInfo", function(x, ...) standardGeneric("stripDebugInfo"))
+
+
+setGeneric("getTypes", function(x, ...) standardGeneric("getTypes"))
+
+setGeneric("getFilename", function(x, ...) standardGeneric("getFilename"))
+setGeneric("getDirectory", function(x, ...) standardGeneric("getDirectory"))
+
+setGeneric("getType",
+            function(obj, ...)
+              standardGeneric("getType"))
+
+# setGeneric("getExternalFunctions", function(x, ...) standardGeneric("getExternalFunctions"))
+# See findCalledFunctions.R
+
+setGeneric("hasError", function(x, ...) standardGeneric("hasError"))
