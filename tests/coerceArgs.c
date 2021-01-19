50f1c587bf762b52ebb016319df56ad93cefaa62
Duncan Temple Lang <duncan@r-project.org>
2021-01-19 06:51:11 -0800

    c code underlying example of coercing arguments in .llvm

---
+++ tests/coerceArgs.c
@@ -0,0 +1,29 @@
+
+double f(double x)
+{
+    return(x + 1);
+}
+
+
+int i(int x)
+{
+    return(x + 1);
+}
+
+
+#include <stdbool.h>
+
+bool b(bool x)
+{
+    return(!x);
+}
+
+bool b0(bool x)
+{
+    return(x);
+}
+
+int b2(bool x)
+{
+    return((int) x);
+}
