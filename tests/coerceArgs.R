630544b2e45a837cd81dd932a57f609be1619fca
Duncan Temple Lang <duncan@r-project.org>
2021-01-19 07:10:09 -0800

    more tests, for 1-bit integer parameter types

--- tests/coerceArgs.R
+++ tests/coerceArgs.R
@@ -7,3 +7,22 @@ m = parseIR("coerceArgs.ir")
 # or identical(.llvm(m$f, 3L), 1) is TRUE
 stopifnot(identical(.llvm(m$f, 3L), .llvm(m$f, 3)))
 
+
+# Now coercing a numeric to a int
+stopifnot(identical(.llvm(m$i, 3L), .llvm(m$i, 3)))
+
+
+stopifnot(identical(.llvm(m$b, TRUE), .llvm(m$b, 1)))
+
+stopifnot(identical(.llvm(m$b2, TRUE), .llvm(m$b2, 1)))
+
+stopifnot(identical(.llvm(m$b0, 0), FALSE))
+stopifnot(identical(.llvm(m$b0, 1), TRUE))
+stopifnot(identical(.llvm(m$b0, 10L), TRUE))
+stopifnot(identical(.llvm(m$b0, 2L), TRUE))
+stopifnot(identical(.llvm(m$b0, 11L), TRUE))
+
+
+
+stopifnot(identical(.llvm(m$b2, 10L) , 1L))
+
