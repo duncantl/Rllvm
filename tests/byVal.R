d48a470b5e5ac0c35c65bcc040d4e55f6681d9ba
Duncan Temple Lang <duncan@r-project.org>
2021-02-14 21:15:29 -0800

    ...

--- tests/byVal.R
+++ tests/byVal.R
@@ -5,7 +5,7 @@ library(Rllvm)
 mod = Module()
 CXCursorType = structType(list(kind = Int32Type, xdata = Int32Type, data = arrayType(pointerType(Int8Type), 3L)), "CXCursor")
 
-gn = Function("clang_CXCursor_getName", StringType, list(CXCursorType, Int32Type, pointerType(Int32Type), pointerType(CXCursorType)), module = mod)
+gn = Function("clang_CXCursor_getKind", StringType, list(CXCursorType, Int32Type, pointerType(Int32Type), pointerType(CXCursorType)), module = mod)
 gn[[1]]
 
 byVal(gn[[1]]) # TRUE
@@ -14,7 +14,8 @@ byVal(gn[[4]]) # FALSE
 a = gn[[4]]
 byVal(a) = TRUE
 
-# The following causes problems as there is no method for [[<-(gn, 4) 
+# The following causes problems as there is no method for [[<-(gn, 4)
+#XXX Fix.
 # byVal(gn[[4]]) = TRUE 
 
  # These two are FALSE.
