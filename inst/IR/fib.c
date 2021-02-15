4866fac70b78ed4c55374dc20844fa3e7d82f54a
Duncan Temple Lang <duncan@r-project.org>
2021-02-14 22:06:24 -0800

    examples for Function with attributes on the Function

--- inst/IR/fib.c
+++ inst/IR/fib.c
@@ -6,3 +6,20 @@ fib(int n)
 
     return(fib(n - 1 ) + fib(n - 2));
 }
+
+
+
+#include <math.h>
+
+int
+fib2(int n)
+{
+    double root5 = 2.2360679774998;
+
+    double phi = (1. + root5)/2.;
+    double psi = 1. - phi;
+
+    double ans = (pow(phi, n) - pow(psi, n))/root5;
+    
+    return(ans);
+}
