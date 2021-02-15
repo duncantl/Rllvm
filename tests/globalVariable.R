1f597eb2194dae028908b0cfa3ecfd093a5af019
Duncan Temple Lang <duncan@r-project.org>
2021-02-14 22:14:20 -0800

    check global variables

--- tests/globalVariable.R
+++ tests/globalVariable.R
@@ -1,8 +1,11 @@
 library(Rllvm)
-m = parseIR("AnalyzeCCode/simple.ll")
-getValue(m[[".str.2"]])
-getValue(m[[".str.1"]])
+m = parseIR("../AnalyzeCCode/simple.ll")
+a = getValue(m[[".str.2"]])
+stopifnot(a == "xyz")
+b = getValue(m[[".str.1"]])
+stopifnot(b == "bcde and fgh")
 
 names(m)
-sapply(grep("^\\.str", names(m), value = TRUE), function(id) getValue(m[[id]]))
+ans = sapply(grep("^\\.str", names(m), value = TRUE), function(id) getValue(m[[id]]))
+stopifnot(identical(ans , c(".str" = "a", ".str.1" = "bcde and fgh", ".str.2" = "xyz")))
 
