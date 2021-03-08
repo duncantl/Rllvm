9ea640f5dc3dd3c6d46cd32b8fce8e2d7b556376
Duncan Temple Lang <duncan@r-project.org>
2021-03-07 20:02:32 -0800

    changed the name of a variable from tu to passtu so both can exist in the same session

--- TU/passes.R
+++ TU/passes.R
@@ -1,12 +1,12 @@
 library(RCIndex)
-tu = createTU("passes.cc", includes = c("~/local/include",
+passtu = createTU("passes.cc", includes = c("~/local/include",
                                       "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/",
                                       "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/c++/4.2.1/"),
                 args = c("-D__STDC_LIMIT_MACROS=1", "-D__STDC_CONSTANT_MACROS=1"))
 
-top = as(tu, "CXCursor")
+top = as(passtu, "CXCursor")
 
-top[[4]] # llvm namespace for first include
+#???? top[[4]] # llvm namespace for first include
 
 rr = getRoutines(top[[4]])
 
