8418411ef367db84e5d1243147f8dd2383c0a438
Duncan Temple Lang <duncan@r-project.org>
2021-11-18 12:26:42 -0800

    added enums for LLVM 13.0

---
+++ R/z_enumDefs_13.0.R
@@ -0,0 +1,682 @@
+if(llvmCheckVersion(c( 13 ,  0 ))) {
+
+`UnaryOpsBegin` = 0L
+`FNeg` = 12L
+`UnaryOpsEnd` = 13L
+
+
+
+ `UnaryOps`  = c(UnaryOpsBegin = 0L, FNeg = 12L, UnaryOpsEnd = 13L)
+
+
+##########################
+
+`Xchg` = 0L
+`Add` = 1L
+`Sub` = 2L
+`And` = 3L
+`Nand` = 4L
+`Or` = 5L
+`Xor` = 6L
+`Max` = 7L
+`Min` = 8L
+`UMax` = 9L
+`UMin` = 10L
+`FAdd` = 11L
+`FSub` = 12L
+`FIRST_BINOP` = 0L
+`LAST_BINOP` = 12L
+`BAD_BINOP` = 13L
+
+
+
+ `BinOp`  = c(Xchg = 0L, Add = 1L, Sub = 2L, And = 3L, Nand = 4L, Or = 5L, 
+Xor = 6L, Max = 7L, Min = 8L, UMax = 9L, UMin = 10L, FAdd = 11L, 
+FSub = 12L, FIRST_BINOP = 0L, LAST_BINOP = 12L, BAD_BINOP = 13L
+)
+
+
+##########################
+
+`BinaryOpsBegin` = 0L
+`Add` = 13L
+`FAdd` = 14L
+`Sub` = 15L
+`FSub` = 16L
+`Mul` = 17L
+`FMul` = 18L
+`UDiv` = 19L
+`SDiv` = 20L
+`FDiv` = 21L
+`URem` = 22L
+`SRem` = 23L
+`FRem` = 24L
+`Shl` = 25L
+`LShr` = 26L
+`AShr` = 27L
+`And` = 28L
+`Or` = 29L
+`Xor` = 30L
+`BinaryOpsEnd` = 31L
+
+
+
+ `BinaryOps`  = c(BinaryOpsBegin = 0L, Add = 13L, FAdd = 14L, Sub = 15L, FSub = 16L, 
+Mul = 17L, FMul = 18L, UDiv = 19L, SDiv = 20L, FDiv = 21L, URem = 22L, 
+SRem = 23L, FRem = 24L, Shl = 25L, LShr = 26L, AShr = 27L, And = 28L, 
+Or = 29L, Xor = 30L, BinaryOpsEnd = 31L)
+
+
+##########################
+
+`CastOpsBegin` = 0L
+`Trunc` = 38L
+`ZExt` = 39L
+`SExt` = 40L
+`FPToUI` = 41L
+`FPToSI` = 42L
+`UIToFP` = 43L
+`SIToFP` = 44L
+`FPTrunc` = 45L
+`FPExt` = 46L
+`PtrToInt` = 47L
+`IntToPtr` = 48L
+`BitCast` = 49L
+`AddrSpaceCast` = 50L
+`CastOpsEnd` = 51L
+
+
+
+ `CastOps`  = c(CastOpsBegin = 0L, Trunc = 38L, ZExt = 39L, SExt = 40L, FPToUI = 41L, 
+FPToSI = 42L, UIToFP = 43L, SIToFP = 44L, FPTrunc = 45L, FPExt = 46L, 
+PtrToInt = 47L, IntToPtr = 48L, BitCast = 49L, AddrSpaceCast = 50L, 
+CastOpsEnd = 51L)
+
+
+##########################
+
+`None` = 0L
+`FirstEnumAttr` = 1L
+`AlwaysInline` = 1L
+`ArgMemOnly` = 2L
+`Builtin` = 3L
+`Cold` = 4L
+`Convergent` = 5L
+`Hot` = 6L
+`ImmArg` = 7L
+`InReg` = 8L
+`InaccessibleMemOnly` = 9L
+`InaccessibleMemOrArgMemOnly` = 10L
+`InlineHint` = 11L
+`JumpTable` = 12L
+`MinSize` = 13L
+`MustProgress` = 14L
+`Naked` = 15L
+`Nest` = 16L
+`NoAlias` = 17L
+`NoBuiltin` = 18L
+`NoCallback` = 19L
+`NoCapture` = 20L
+`NoCfCheck` = 21L
+`NoDuplicate` = 22L
+`NoFree` = 23L
+`NoImplicitFloat` = 24L
+`NoInline` = 25L
+`NoMerge` = 26L
+`NoProfile` = 27L
+`NoRecurse` = 28L
+`NoRedZone` = 29L
+`NoReturn` = 30L
+`NoSanitizeCoverage` = 31L
+`NoSync` = 32L
+`NoUndef` = 33L
+`NoUnwind` = 34L
+`NonLazyBind` = 35L
+`NonNull` = 36L
+`NullPointerIsValid` = 37L
+`OptForFuzzing` = 38L
+`OptimizeForSize` = 39L
+`OptimizeNone` = 40L
+`ReadNone` = 41L
+`ReadOnly` = 42L
+`Returned` = 43L
+`ReturnsTwice` = 44L
+`SExt` = 45L
+`SafeStack` = 46L
+`SanitizeAddress` = 47L
+`SanitizeHWAddress` = 48L
+`SanitizeMemTag` = 49L
+`SanitizeMemory` = 50L
+`SanitizeThread` = 51L
+`ShadowCallStack` = 52L
+`Speculatable` = 53L
+`SpeculativeLoadHardening` = 54L
+`StackProtect` = 55L
+`StackProtectReq` = 56L
+`StackProtectStrong` = 57L
+`StrictFP` = 58L
+`SwiftAsync` = 59L
+`SwiftError` = 60L
+`SwiftSelf` = 61L
+`UWTable` = 62L
+`WillReturn` = 63L
+`WriteOnly` = 64L
+`ZExt` = 65L
+`LastEnumAttr` = 65L
+`FirstTypeAttr` = 66L
+`ByRef` = 66L
+`ByVal` = 67L
+`ElementType` = 68L
+`InAlloca` = 69L
+`Preallocated` = 70L
+`StructRet` = 71L
+`LastTypeAttr` = 71L
+`FirstIntAttr` = 72L
+`Alignment` = 72L
+`AllocSize` = 73L
+`Dereferenceable` = 74L
+`DereferenceableOrNull` = 75L
+`StackAlignment` = 76L
+`VScaleRange` = 77L
+`LastIntAttr` = 77L
+`EndAttrKinds` = 78L
+`EmptyKey` = 79L
+`TombstoneKey` = 80L
+
+
+
+ `AttrKind`  = c(None = 0L, FirstEnumAttr = 1L, AlwaysInline = 1L, ArgMemOnly = 2L, 
+Builtin = 3L, Cold = 4L, Convergent = 5L, Hot = 6L, ImmArg = 7L, 
+InReg = 8L, InaccessibleMemOnly = 9L, InaccessibleMemOrArgMemOnly = 10L, 
+InlineHint = 11L, JumpTable = 12L, MinSize = 13L, MustProgress = 14L, 
+Naked = 15L, Nest = 16L, NoAlias = 17L, NoBuiltin = 18L, NoCallback = 19L, 
+NoCapture = 20L, NoCfCheck = 21L, NoDuplicate = 22L, NoFree = 23L, 
+NoImplicitFloat = 24L, NoInline = 25L, NoMerge = 26L, NoProfile = 27L, 
+NoRecurse = 28L, NoRedZone = 29L, NoReturn = 30L, NoSanitizeCoverage = 31L, 
+NoSync = 32L, NoUndef = 33L, NoUnwind = 34L, NonLazyBind = 35L, 
+NonNull = 36L, NullPointerIsValid = 37L, OptForFuzzing = 38L, 
+OptimizeForSize = 39L, OptimizeNone = 40L, ReadNone = 41L, ReadOnly = 42L, 
+Returned = 43L, ReturnsTwice = 44L, SExt = 45L, SafeStack = 46L, 
+SanitizeAddress = 47L, SanitizeHWAddress = 48L, SanitizeMemTag = 49L, 
+SanitizeMemory = 50L, SanitizeThread = 51L, ShadowCallStack = 52L, 
+Speculatable = 53L, SpeculativeLoadHardening = 54L, StackProtect = 55L, 
+StackProtectReq = 56L, StackProtectStrong = 57L, StrictFP = 58L, 
+SwiftAsync = 59L, SwiftError = 60L, SwiftSelf = 61L, UWTable = 62L, 
+WillReturn = 63L, WriteOnly = 64L, ZExt = 65L, LastEnumAttr = 65L, 
+FirstTypeAttr = 66L, ByRef = 66L, ByVal = 67L, ElementType = 68L, 
+InAlloca = 69L, Preallocated = 70L, StructRet = 71L, LastTypeAttr = 71L, 
+FirstIntAttr = 72L, Alignment = 72L, AllocSize = 73L, Dereferenceable = 74L, 
+DereferenceableOrNull = 75L, StackAlignment = 76L, VScaleRange = 77L, 
+LastIntAttr = 77L, EndAttrKinds = 78L, EmptyKey = 79L, TombstoneKey = 80L
+)
+
+
+##########################
+
+`MDStringKind` = 0L
+`ConstantAsMetadataKind` = 1L
+`LocalAsMetadataKind` = 2L
+`DistinctMDOperandPlaceholderKind` = 3L
+`MDTupleKind` = 4L
+`DILocationKind` = 5L
+`DIExpressionKind` = 6L
+`DIGlobalVariableExpressionKind` = 7L
+`GenericDINodeKind` = 8L
+`DISubrangeKind` = 9L
+`DIEnumeratorKind` = 10L
+`DIBasicTypeKind` = 11L
+`DIDerivedTypeKind` = 12L
+`DICompositeTypeKind` = 13L
+`DISubroutineTypeKind` = 14L
+`DIFileKind` = 15L
+`DICompileUnitKind` = 16L
+`DISubprogramKind` = 17L
+`DILexicalBlockKind` = 18L
+`DILexicalBlockFileKind` = 19L
+`DINamespaceKind` = 20L
+`DIModuleKind` = 21L
+`DITemplateTypeParameterKind` = 22L
+`DITemplateValueParameterKind` = 23L
+`DIGlobalVariableKind` = 24L
+`DILocalVariableKind` = 25L
+`DILabelKind` = 26L
+`DIObjCPropertyKind` = 27L
+`DIImportedEntityKind` = 28L
+`DIMacroKind` = 29L
+`DIMacroFileKind` = 30L
+`DICommonBlockKind` = 31L
+`DIArgListKind` = 32L
+`DIStringTypeKind` = 33L
+`DIGenericSubrangeKind` = 34L
+
+
+
+ `MetadataKind`  = structure(0:34, .Names = c("MDStringKind", "ConstantAsMetadataKind", 
+"LocalAsMetadataKind", "DistinctMDOperandPlaceholderKind", "MDTupleKind", 
+"DILocationKind", "DIExpressionKind", "DIGlobalVariableExpressionKind", 
+"GenericDINodeKind", "DISubrangeKind", "DIEnumeratorKind", "DIBasicTypeKind", 
+"DIDerivedTypeKind", "DICompositeTypeKind", "DISubroutineTypeKind", 
+"DIFileKind", "DICompileUnitKind", "DISubprogramKind", "DILexicalBlockKind", 
+"DILexicalBlockFileKind", "DINamespaceKind", "DIModuleKind", 
+"DITemplateTypeParameterKind", "DITemplateValueParameterKind", 
+"DIGlobalVariableKind", "DILocalVariableKind", "DILabelKind", 
+"DIObjCPropertyKind", "DIImportedEntityKind", "DIMacroKind", 
+"DIMacroFileKind", "DICommonBlockKind", "DIArgListKind", "DIStringTypeKind", 
+"DIGenericSubrangeKind"))
+
+
+##########################
+
+`TermOpsBegin` = 0L
+`Ret` = 1L
+`Br` = 2L
+`Switch` = 3L
+`IndirectBr` = 4L
+`Invoke` = 5L
+`Resume` = 6L
+`Unreachable` = 7L
+`CleanupRet` = 8L
+`CatchRet` = 9L
+`CatchSwitch` = 10L
+`CallBr` = 11L
+`TermOpsEnd` = 12L
+
+
+
+ `TermOps`  = structure(0:12, .Names = c("TermOpsBegin", "Ret", "Br", "Switch", 
+"IndirectBr", "Invoke", "Resume", "Unreachable", "CleanupRet", 
+"CatchRet", "CatchSwitch", "CallBr", "TermOpsEnd"))
+
+
+##########################
+
+`ExternalLinkage` = 0L
+`AvailableExternallyLinkage` = 1L
+`LinkOnceAnyLinkage` = 2L
+`LinkOnceODRLinkage` = 3L
+`WeakAnyLinkage` = 4L
+`WeakODRLinkage` = 5L
+`AppendingLinkage` = 6L
+`InternalLinkage` = 7L
+`PrivateLinkage` = 8L
+`ExternalWeakLinkage` = 9L
+`CommonLinkage` = 10L
+
+
+
+ `LinkageTypes`  = structure(0:10, .Names = c("ExternalLinkage", "AvailableExternallyLinkage", 
+"LinkOnceAnyLinkage", "LinkOnceODRLinkage", "WeakAnyLinkage", 
+"WeakODRLinkage", "AppendingLinkage", "InternalLinkage", "PrivateLinkage", 
+"ExternalWeakLinkage", "CommonLinkage"))
+
+
+##########################
+
+`LLVMCodeGenLevelNone` = 0L
+`LLVMCodeGenLevelLess` = 1L
+`LLVMCodeGenLevelDefault` = 2L
+`LLVMCodeGenLevelAggressive` = 3L
+
+
+
+ `LLVMCodeGenOptLevel`  = structure(0:3, .Names = c("LLVMCodeGenLevelNone", "LLVMCodeGenLevelLess", 
+"LLVMCodeGenLevelDefault", "LLVMCodeGenLevelAggressive"))
+
+
+##########################
+
+`FCMP_FALSE` = 0L
+`FCMP_OEQ` = 1L
+`FCMP_OGT` = 2L
+`FCMP_OGE` = 3L
+`FCMP_OLT` = 4L
+`FCMP_OLE` = 5L
+`FCMP_ONE` = 6L
+`FCMP_ORD` = 7L
+`FCMP_UNO` = 8L
+`FCMP_UEQ` = 9L
+`FCMP_UGT` = 10L
+`FCMP_UGE` = 11L
+`FCMP_ULT` = 12L
+`FCMP_ULE` = 13L
+`FCMP_UNE` = 14L
+`FCMP_TRUE` = 15L
+`FIRST_FCMP_PREDICATE` = 0L
+`LAST_FCMP_PREDICATE` = 15L
+`BAD_FCMP_PREDICATE` = NA
+`ICMP_EQ` = 32L
+`ICMP_NE` = 33L
+`ICMP_UGT` = 34L
+`ICMP_UGE` = 35L
+`ICMP_ULT` = 36L
+`ICMP_ULE` = 37L
+`ICMP_SGT` = 38L
+`ICMP_SGE` = 39L
+`ICMP_SLT` = 40L
+`ICMP_SLE` = 41L
+`FIRST_ICMP_PREDICATE` = 32L
+`LAST_ICMP_PREDICATE` = 41L
+`BAD_ICMP_PREDICATE` = NA
+
+
+
+ `Predicate`  = c(FCMP_FALSE = 0L, FCMP_OEQ = 1L, FCMP_OGT = 2L, FCMP_OGE = 3L, 
+FCMP_OLT = 4L, FCMP_OLE = 5L, FCMP_ONE = 6L, FCMP_ORD = 7L, FCMP_UNO = 8L, 
+FCMP_UEQ = 9L, FCMP_UGT = 10L, FCMP_UGE = 11L, FCMP_ULT = 12L, 
+FCMP_ULE = 13L, FCMP_UNE = 14L, FCMP_TRUE = 15L, FIRST_FCMP_PREDICATE = 0L, 
+LAST_FCMP_PREDICATE = 15L, BAD_FCMP_PREDICATE = NA, ICMP_EQ = 32L, 
+ICMP_NE = 33L, ICMP_UGT = 34L, ICMP_UGE = 35L, ICMP_ULT = 36L, 
+ICMP_ULE = 37L, ICMP_SGT = 38L, ICMP_SGE = 39L, ICMP_SLT = 40L, 
+ICMP_SLE = 41L, FIRST_ICMP_PREDICATE = 32L, LAST_ICMP_PREDICATE = 41L, 
+BAD_ICMP_PREDICATE = NA)
+
+
+##########################
+
+`TCK_None` = 0L
+`TCK_Tail` = 1L
+`TCK_MustTail` = 2L
+`TCK_NoTail` = 3L
+`TCK_LAST` = 3L
+
+
+
+ `TailCallKind`  = c(TCK_None = 0L, TCK_Tail = 1L, TCK_MustTail = 2L, TCK_NoTail = 3L, 
+TCK_LAST = 3L)
+
+
+##########################
+
+`FlagZero` = 0L
+`FlagPrivate` = 1L
+`FlagProtected` = 2L
+`FlagPublic` = 3L
+`FlagFwdDecl` = 4L
+`FlagAppleBlock` = 5L
+`FlagReservedBit4` = 6L
+`FlagVirtual` = 7L
+`FlagArtificial` = 8L
+`FlagExplicit` = 9L
+`FlagPrototyped` = 10L
+`FlagObjcClassComplete` = 11L
+`FlagObjectPointer` = 12L
+`FlagVector` = 13L
+`FlagStaticMember` = 14L
+`FlagLValueReference` = 15L
+`FlagRValueReference` = 16L
+`FlagExportSymbols` = 17L
+`FlagSingleInheritance` = 18L
+`FlagMultipleInheritance` = 19L
+`FlagVirtualInheritance` = 20L
+`FlagIntroducedVirtual` = 21L
+`FlagBitField` = 22L
+`FlagNoReturn` = 23L
+`FlagTypePassByValue` = 24L
+`FlagTypePassByReference` = 25L
+`FlagEnumClass` = 26L
+`FlagThunk` = 27L
+`FlagNonTrivial` = 28L
+`FlagBigEndian` = 29L
+`FlagLittleEndian` = 30L
+`FlagAllCallsDescribed` = 31L
+`FlagIndirectVirtualBase` = 32L
+`FlagLargest` = 33L
+`FlagAccessibility` = 3L
+`FlagPtrToMemberRep` = 23L
+`LLVM_BITMASK_LARGEST_ENUMERATOR` = 24L
+
+
+
+ `DIFlags`  = c(FlagZero = 0, FlagPrivate = 1, FlagProtected = 2, FlagPublic = 3, 
+FlagFwdDecl = 4, FlagAppleBlock = 5, FlagReservedBit4 = 6, FlagVirtual = 7, 
+FlagArtificial = 8, FlagExplicit = 9, FlagPrototyped = 10, FlagObjcClassComplete = 11, 
+FlagObjectPointer = 12, FlagVector = 13, FlagStaticMember = 14, 
+FlagLValueReference = 15, FlagRValueReference = 16, FlagExportSymbols = 17, 
+FlagSingleInheritance = 18, FlagMultipleInheritance = 19, FlagVirtualInheritance = 20, 
+FlagIntroducedVirtual = 21, FlagBitField = 22, FlagNoReturn = 23, 
+FlagTypePassByValue = 24, FlagTypePassByReference = 25, FlagEnumClass = 26, 
+FlagThunk = 27, FlagNonTrivial = 28, FlagBigEndian = 29, FlagLittleEndian = 30, 
+FlagAllCallsDescribed = 31, FlagIndirectVirtualBase = 32, FlagLargest = 33, 
+FlagAccessibility = 3, FlagPtrToMemberRep = 23, LLVM_BITMASK_LARGEST_ENUMERATOR = 24
+)
+
+
+##########################
+
+`HalfTyID` = 0L
+`BFloatTyID` = 1L
+`FloatTyID` = 2L
+`DoubleTyID` = 3L
+`X86_FP80TyID` = 4L
+`FP128TyID` = 5L
+`PPC_FP128TyID` = 6L
+`VoidTyID` = 7L
+`LabelTyID` = 8L
+`MetadataTyID` = 9L
+`X86_MMXTyID` = 10L
+`X86_AMXTyID` = 11L
+`TokenTyID` = 12L
+`IntegerTyID` = 13L
+`FunctionTyID` = 14L
+`PointerTyID` = 15L
+`StructTyID` = 16L
+`ArrayTyID` = 17L
+`FixedVectorTyID` = 18L
+`ScalableVectorTyID` = 19L
+
+
+
+ `TypeID`  = structure(0:19, .Names = c("HalfTyID", "BFloatTyID", "FloatTyID", 
+"DoubleTyID", "X86_FP80TyID", "FP128TyID", "PPC_FP128TyID", "VoidTyID", 
+"LabelTyID", "MetadataTyID", "X86_MMXTyID", "X86_AMXTyID", "TokenTyID", 
+"IntegerTyID", "FunctionTyID", "PointerTyID", "StructTyID", "ArrayTyID", 
+"FixedVectorTyID", "ScalableVectorTyID"))
+
+
+##########################
+
+`DW_CC_normal` = 0L
+`DW_CC_program` = 1L
+`DW_CC_nocall` = 2L
+`DW_CC_pass_by_reference` = 3L
+`DW_CC_pass_by_value` = 4L
+`DW_CC_GNU_renesas_sh` = 5L
+`DW_CC_GNU_borland_fastcall_i386` = 6L
+`DW_CC_BORLAND_safecall` = 7L
+`DW_CC_BORLAND_stdcall` = 8L
+`DW_CC_BORLAND_pascal` = 9L
+`DW_CC_BORLAND_msfastcall` = 10L
+`DW_CC_BORLAND_msreturn` = 11L
+`DW_CC_BORLAND_thiscall` = 12L
+`DW_CC_BORLAND_fastcall` = 13L
+`DW_CC_LLVM_vectorcall` = 14L
+`DW_CC_LLVM_Win64` = 15L
+`DW_CC_LLVM_X86_64SysV` = 16L
+`DW_CC_LLVM_AAPCS` = 17L
+`DW_CC_LLVM_AAPCS_VFP` = 18L
+`DW_CC_LLVM_IntelOclBicc` = 19L
+`DW_CC_LLVM_SpirFunction` = 20L
+`DW_CC_LLVM_OpenCLKernel` = 21L
+`DW_CC_LLVM_Swift` = 22L
+`DW_CC_LLVM_PreserveMost` = 23L
+`DW_CC_LLVM_PreserveAll` = 24L
+`DW_CC_LLVM_X86RegCall` = 25L
+`DW_CC_GDB_IBM_OpenCL` = 26L
+`DW_CC_lo_user` = 64L
+`DW_CC_hi_user` = 255L
+
+
+
+ `CallingConvention`  = c(DW_CC_normal = 0L, DW_CC_program = 1L, DW_CC_nocall = 2L, DW_CC_pass_by_reference = 3L, 
+DW_CC_pass_by_value = 4L, DW_CC_GNU_renesas_sh = 5L, DW_CC_GNU_borland_fastcall_i386 = 6L, 
+DW_CC_BORLAND_safecall = 7L, DW_CC_BORLAND_stdcall = 8L, DW_CC_BORLAND_pascal = 9L, 
+DW_CC_BORLAND_msfastcall = 10L, DW_CC_BORLAND_msreturn = 11L, 
+DW_CC_BORLAND_thiscall = 12L, DW_CC_BORLAND_fastcall = 13L, DW_CC_LLVM_vectorcall = 14L, 
+DW_CC_LLVM_Win64 = 15L, DW_CC_LLVM_X86_64SysV = 16L, DW_CC_LLVM_AAPCS = 17L, 
+DW_CC_LLVM_AAPCS_VFP = 18L, DW_CC_LLVM_IntelOclBicc = 19L, DW_CC_LLVM_SpirFunction = 20L, 
+DW_CC_LLVM_OpenCLKernel = 21L, DW_CC_LLVM_Swift = 22L, DW_CC_LLVM_PreserveMost = 23L, 
+DW_CC_LLVM_PreserveAll = 24L, DW_CC_LLVM_X86RegCall = 25L, DW_CC_GDB_IBM_OpenCL = 26L, 
+DW_CC_lo_user = 64L, DW_CC_hi_user = 255L)
+
+
+##########################
+
+`None` = 0L
+`Local` = 1L
+`Global` = 2L
+
+
+
+ `UnnamedAddr`  = structure(0:2, .Names = c("None", "Local", "Global"))
+
+
+##########################
+
+`DW_LANG_C89` = 0L
+`DW_LANG_C` = 1L
+`DW_LANG_Ada83` = 2L
+`DW_LANG_C_plus_plus` = 3L
+`DW_LANG_Cobol74` = 4L
+`DW_LANG_Cobol85` = 5L
+`DW_LANG_Fortran77` = 6L
+`DW_LANG_Fortran90` = 7L
+`DW_LANG_Pascal83` = 8L
+`DW_LANG_Modula2` = 9L
+`DW_LANG_Java` = 10L
+`DW_LANG_C99` = 11L
+`DW_LANG_Ada95` = 12L
+`DW_LANG_Fortran95` = 13L
+`DW_LANG_PLI` = 14L
+`DW_LANG_ObjC` = 15L
+`DW_LANG_ObjC_plus_plus` = 16L
+`DW_LANG_UPC` = 17L
+`DW_LANG_D` = 18L
+`DW_LANG_Python` = 19L
+`DW_LANG_OpenCL` = 20L
+`DW_LANG_Go` = 21L
+`DW_LANG_Modula3` = 22L
+`DW_LANG_Haskell` = 23L
+`DW_LANG_C_plus_plus_03` = 24L
+`DW_LANG_C_plus_plus_11` = 25L
+`DW_LANG_OCaml` = 26L
+`DW_LANG_Rust` = 27L
+`DW_LANG_C11` = 28L
+`DW_LANG_Swift` = 29L
+`DW_LANG_Julia` = 30L
+`DW_LANG_Dylan` = 31L
+`DW_LANG_C_plus_plus_14` = 32L
+`DW_LANG_Fortran03` = 33L
+`DW_LANG_Fortran08` = 34L
+`DW_LANG_RenderScript` = 35L
+`DW_LANG_BLISS` = 36L
+`DW_LANG_Mips_Assembler` = 37L
+`DW_LANG_GOOGLE_RenderScript` = 38L
+`DW_LANG_BORLAND_Delphi` = 39L
+`DW_LANG_lo_user` = 32768L
+`DW_LANG_hi_user` = 65535L
+
+
+
+ `SourceLanguage`  = c(DW_LANG_C89 = 0L, DW_LANG_C = 1L, DW_LANG_Ada83 = 2L, DW_LANG_C_plus_plus = 3L, 
+DW_LANG_Cobol74 = 4L, DW_LANG_Cobol85 = 5L, DW_LANG_Fortran77 = 6L, 
+DW_LANG_Fortran90 = 7L, DW_LANG_Pascal83 = 8L, DW_LANG_Modula2 = 9L, 
+DW_LANG_Java = 10L, DW_LANG_C99 = 11L, DW_LANG_Ada95 = 12L, DW_LANG_Fortran95 = 13L, 
+DW_LANG_PLI = 14L, DW_LANG_ObjC = 15L, DW_LANG_ObjC_plus_plus = 16L, 
+DW_LANG_UPC = 17L, DW_LANG_D = 18L, DW_LANG_Python = 19L, DW_LANG_OpenCL = 20L, 
+DW_LANG_Go = 21L, DW_LANG_Modula3 = 22L, DW_LANG_Haskell = 23L, 
+DW_LANG_C_plus_plus_03 = 24L, DW_LANG_C_plus_plus_11 = 25L, DW_LANG_OCaml = 26L, 
+DW_LANG_Rust = 27L, DW_LANG_C11 = 28L, DW_LANG_Swift = 29L, DW_LANG_Julia = 30L, 
+DW_LANG_Dylan = 31L, DW_LANG_C_plus_plus_14 = 32L, DW_LANG_Fortran03 = 33L, 
+DW_LANG_Fortran08 = 34L, DW_LANG_RenderScript = 35L, DW_LANG_BLISS = 36L, 
+DW_LANG_Mips_Assembler = 37L, DW_LANG_GOOGLE_RenderScript = 38L, 
+DW_LANG_BORLAND_Delphi = 39L, DW_LANG_lo_user = 32768L, DW_LANG_hi_user = 65535L
+)
+
+
+##########################
+
+`Increasing` = 0L
+`Decreasing` = 1L
+`Unknown` = 2L
+
+
+
+ `Direction`  = structure(0:2, .Names = c("Increasing", "Decreasing", "Unknown"
+))
+
+
+##########################
+
+`DefaultVisibility` = 0L
+`HiddenVisibility` = 1L
+`ProtectedVisibility` = 2L
+
+
+
+ `VisibilityTypes`  = structure(0:2, .Names = c("DefaultVisibility", "HiddenVisibility", 
+"ProtectedVisibility"))
+
+
+##########################
+
+`DefaultStorageClass` = 0L
+`DLLImportStorageClass` = 1L
+`DLLExportStorageClass` = 2L
+
+
+
+ `DLLStorageClassTypes`  = structure(0:2, .Names = c("DefaultStorageClass", "DLLImportStorageClass", 
+"DLLExportStorageClass"))
+
+
+##########################
+
+`NotThreadLocal` = 0L
+`GeneralDynamicTLSModel` = 1L
+`LocalDynamicTLSModel` = 2L
+`InitialExecTLSModel` = 3L
+`LocalExecTLSModel` = 4L
+
+
+
+ `ThreadLocalMode`  = structure(0:4, .Names = c("NotThreadLocal", "GeneralDynamicTLSModel", 
+"LocalDynamicTLSModel", "InitialExecTLSModel", "LocalExecTLSModel"
+))
+
+
+##########################
+
+`VCallVisibilityPublic` = 0L
+`VCallVisibilityLinkageUnit` = 1L
+`VCallVisibilityTranslationUnit` = 2L
+
+
+
+ `VCallVisibility`  = structure(0:2, .Names = c("VCallVisibilityPublic", "VCallVisibilityLinkageUnit", 
+"VCallVisibilityTranslationUnit"))
+
+
+##########################
+
+`LLVMRelocDefault` = 0L
+`LLVMRelocStatic` = 1L
+`LLVMRelocPIC` = 2L
+`LLVMRelocDynamicNoPic` = 3L
+`LLVMRelocROPI` = 4L
+`LLVMRelocRWPI` = 5L
+`LLVMRelocROPI_RWPI` = 6L
+
+
+
+ `LLVMRelocMode`  = structure(0:6, .Names = c("LLVMRelocDefault", "LLVMRelocStatic", 
+"LLVMRelocPIC", "LLVMRelocDynamicNoPic", "LLVMRelocROPI", "LLVMRelocRWPI", 
+"LLVMRelocROPI_RWPI"))
+
+
+##########################
+
+
+
+
+}
+
