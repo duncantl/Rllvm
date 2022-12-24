d106d48de2e58797d5c8e2a459039bda67ace357
Duncan Temple Lang <duncan@r-project.org>
2022-12-23 19:10:26 -0800

    added a select instruction but that seems to do both sides

--- explorations/dnormLoop.R
+++ explorations/dnormLoop.R
@@ -2,12 +2,19 @@ library(Rllvm)
 m = parseIR("dnormLoop.ir")
 
 if(FALSE) {
+    # Run but it doesn't copy x.
     x0 = x = seq(-1.5, 1.5, by = .1)
-
+    z = .llvm(m$v_dnorm, x, length(x), 0, 1, .duplicate = c(TRUE, FALSE, FALSE, FALSE))
+    
     # will write directly into x0 and x which are the same.
     z = .llvm(m$v_dnorm, x, length(x), 0, 1, .duplicate = c(TRUE, FALSE, FALSE, FALSE))
 }
 
+
+
+# Build a proxy function that we can invoke via .Call().
+# It can replace the coercion that .llvm() needs to do
+# 
 
 p = getParameters(m$v_dnorm)
 rr = simpleFunction( "R_v_dnorm", SEXPType, .types = replicate(length(p), SEXPType), module = m)
@@ -22,27 +29,29 @@ duplicate = Function("Rf_duplicate", SEXPType, list(SEXPType), module = m)
 printValue = Function("Rf_PrintValue", VoidType, list(SEXPType), module = m)
 protect = Function("Rf_protect", SEXPType, list(SEXPType), module = m)
 unprotect = Function("Rf_unprotect", VoidType, list(Int32Type), module = m)
-allocVector = Function("Rf_allocVector", SEXPType, list(Int32Type, Int32Type), module = m)
-allocVector3 = Function("Rf_allocVector3", SEXPType, list(Int32Type, Int32Type, pointerType(VoidType)), module = m)
-ScalarInteger = Function("Rf_ScalarInteger", SEXPType, list(Int32Type), module = m)
+typeof = Function("TYPEOF", Int32Type, list(SEXPType), module = m)        
+#allocVector = Function("Rf_allocVector", SEXPType, list(Int32Type, Int32Type), module = m)
+#allocVector3 = Function("Rf_allocVector3", SEXPType, list(Int32Type, Int32Type, pointerType(VoidType)), module = m)
+#ScalarInteger = Function("Rf_ScalarInteger", SEXPType, list(Int32Type), module = m)
     
 if(TRUE) {    
 
-    xc = ir$createCall(duplicate, pr[[1]])
+    ir$createCall(printValue, pr[[1]])    
+    type = ir$createCall(typeof, pr[[1]])
+    typeCond = ir$createICmp(ICMP_EQ, type, ir$createConstant(14L))
+    # Does this call
+    xc = ir$createSelect(typeCond, ir$createCall(duplicate, pr[[1]]), ir$createCall(coerceVector, pr[[1]], 14L))
     # Not needed in this case.
     # ir$createCall(protect, xc)
-   x2 = ir$createCall(REAL, xc)
-  len = ir$createCall(asInteger, pr[[2]])
-              # nw = ir$createCall(allocVector3, 13L, 4L, 0L)
-              # nw = ir$createCall(allocVector, 13L, 4L)
-   mu = ir$createCall(asReal, pr[[3]])
-   sd = ir$createCall(asReal, pr[[4]])
+    x2 = ir$createCall(REAL, xc)
+    len = ir$createCall(asInteger, pr[[2]])
 
-   ir$createCall(m$v_dnorm, x2, len, mu, sd)
+    mu = ir$createCall(asReal, pr[[3]])
+    sd = ir$createCall(asReal, pr[[4]])
+
+    ir$createCall(m$v_dnorm, x2, len, mu, sd)
 
    # ir$createCall(unprotect, 1L)
-              # ir$createReturn(ir$createCall(ScalarInteger, len))
-              # ir$createReturn(nw)
     ir$createReturn(xc)
 } else {
     ir$createCall(printValue, pr[[1]])
@@ -84,3 +93,12 @@ fptr = getPointerToFunction(rr$fun, ee)@ref
 sym = list(name = "R_v_dnorm", address = structure(fptr, class = "NativeSymbol"), dll = NULL)
 # y = .Call(sym, x, length(x), 0.1, 1.2)
 
+
+truth = c(0.136675077204378, 0.152207587833673, 0.16833225640452, 0.184876815843572, 
+0.201642292524936, 0.218406151494248, 0.234926588580926, 0.250947887623335, 
+0.266206700434743, 0.280439049679568, 0.293387804447713, 0.304810338740607, 
+0.31448605753319, 0.322223465972462, 0.327866466006629, 0.331299591513103, 
+0.332451936758599, 0.331299591513103, 0.327866466006629, 0.322223465972462, 
+0.31448605753319, 0.304810338740607, 0.293387804447713, 0.280439049679568, 
+0.266206700434743, 0.250947887623335, 0.234926588580926, 0.218406151494248, 
+0.201642292524936, 0.184876815843572, 0.16833225640452)
