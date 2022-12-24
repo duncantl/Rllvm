23f19465673e53514b8bcac692f93d7e29bfce71
Duncan Temple Lang <duncan@r-project.org>
2022-12-23 21:19:06 -0800

    .....

--- R/methods.R
+++ R/methods.R
@@ -17,10 +17,17 @@ setMethod("show", "Type",
 setMethod("show", "Function",
           function(object) {
 
-            p = getParameters(object)
-            names(p)[is.na(names(p))] = ""
-            sig = sprintf("%s %s", sapply(p, function(x) getTypeName(getType(x))), names(p))
-            
+              if(identical(object@ref, new("externalptr")))
+                  return()
+
+              
+              p = getParameters(object)
+              if(!is.null(p)) {
+                  names(p)[is.na(names(p))] = ""
+                  sig = sprintf("%s %s", sapply(p, function(x) getTypeName(getType(x))), names(p))
+              } else
+                  sig = ""
+              
             cat(getTypeName(getReturnType(object)),
                 getName(object), "(",
                 paste(sig, collapse = ", "),
