cdae8ec56f881bf2e24ceae0f86f2bb0f9bb8222
Duncan Temple Lang <duncan@r-project.org>
2022-12-29 07:19:18 -0800

    comment

--- explorations/iterators.R
+++ explorations/iterators.R
@@ -2,6 +2,8 @@
 # to iterate over it, we have difficulties in R.
 # We could partition it into blocks stored as elements in  a list.
 #
+# Like AltRep example or Haskell's lazy sequence.
+#
 # We would like to have the efficiency of iterators where we can loop
 # over the elements readily.
 #  sum(seq(1, n))
