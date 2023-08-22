f5ca9eb059cb39a85ceac44a6be2fcfbc193b991
Duncan Temple Lang <duncan@r-project.org>
2023-08-21 19:54:18 -0700

    specify the functions in the Rllvm package as overlap with Rffi

--- tests/gep.R
+++ tests/gep.R
@@ -5,8 +5,8 @@ fun = Function("fun", VoidType, list(), module = module)
 entry = Block(fun, "entry")
 builder = IRBuilder(entry)
 
-data_struct = structType(list(), "data", withNames = FALSE)
+data_struct = Rllvm::structType(list(), "data", withNames = FALSE)
 
-x = createAlloc(builder, arrayType(data_struct, 3L), id = "x")
+x = createAlloc(builder, Rllvm::arrayType(data_struct, 3L), id = "x")
 
 #x_id = createGEP(builder, x, createIntegerConstant(0L), "xxx")
