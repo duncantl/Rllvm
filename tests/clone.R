18e989231e0901885c66727b42fcbeba53f18dba
Duncan Temple Lang <duncan@r-project.org>
2023-08-21 20:01:52 -0700

    comment on what is going on

--- tests/clone.R
+++ tests/clone.R
@@ -5,6 +5,7 @@ source('createGlobal.R')
 # Each time we create a new ExecutionEngine, we effectively reinitialize the
 # value of the global variable gv to -101
 
+# Note there is foo and get.  foo() increments. get just retrieves the current value.
 ee = ExecutionEngine(mod)
 #finalizeEngine(ee)
 start = run(mod$get, .ee = ee)
@@ -14,6 +15,9 @@ end = run(mod$get, .ee = ee)
 m2 = clone(mod)
 message("*** cloned module  **")
 showModule(m2)
+
+cur = run(mod$get, .ee = ee)
+
 
 ee2 = ExecutionEngine(m2)
 #finalizeEngine(ee2)
