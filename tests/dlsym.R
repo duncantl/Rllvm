673a58d5371909c95df48ffcb8ecb3ed795495ad
Duncan Temple Lang <duncan@r-project.org>
2023-08-21 19:59:53 -0700

    comment about possibly having a real.o that is up-to-date but for the wrong architecture

--- tests/dlsym.R
+++ tests/dlsym.R
@@ -6,8 +6,11 @@ stopifnot(!is.null(a) && !Rllvm:::isNativeNull(a))
 e = try(dlsym("R_real", NULL), silent = TRUE) # fails because not in executable
 stopifnot(is(e, 'try-error'))
 
-if(!file.exists("real.so"))
+if(!file.exists("real.so")) {
+    # May need to clean real.o
     system("R CMD SHLIB real.c")
+}
+
 
 if(file.exists("real.so")) {
 
