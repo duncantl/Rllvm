9e4390e1fad4f9b059db73e124e7ebe942aab7d0
Duncan Temple Lang <duncan@r-project.org>
2024-04-21 15:13:54 -0700

    simple additions to display which files were not compiled so can add to a markdown file and record how we resolve the issues.

--- src/Failed.R
+++ src/Failed.R
@@ -1,6 +1,11 @@
 cpp = list.files(pattern = "cpp")
 o = gsub("\\.cpp$", ".o", cpp)
-print(cpp[ !file.exists(o) ])
+fail = cpp[ !file.exists(o) ]
+print(fail)
+
+
+cat(paste("##", fail), sep = "\n\n")
+
 
 # "Pass.cpp"           "RLLVMClassName.cpp"
 # Fixed for 7.0: "Module.cpp"          "Target.cpp"        
