4c29882f3a49cdd0a5c43afeddc51b3bdaf5614c
Sounkou Mahamane Toure <56392505+sounkou-bioinfo@users.noreply.github.com>
2025-08-15 04:03:27 +0400

    ERROR MACRO

--- src/Rllvm.h
+++ src/Rllvm.h
@@ -179,8 +179,8 @@ char const *getDITypeClassName(llvm::MDNode *obj);
 
 #define R_PROBLEM_BUFSIZE	4096
 #define PROBLEM			{char R_problem_buf[R_PROBLEM_BUFSIZE];(snprintf)(R_problem_buf, R_PROBLEM_BUFSIZE,
-#define ERROR			),Rf_error(R_problem_buf);}
-#define WARNING(x)		),Rf_warning(R_problem_buf);}
+#define ERROR			),Rf_error("%s", R_problem_buf);}
+#define WARNING(x)		),Rf_warning("%s", R_problem_buf);}
 #define WARN			WARNING(NULL)
 
 #endif
